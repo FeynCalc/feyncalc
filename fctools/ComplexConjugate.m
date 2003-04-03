cf87277b60dffc23616dae265f6ba53a328fcbcc
Frederik Orellana <frederik@orellana.dk>
2003-04-03 19:13:09 +0000

    Changed to use DOT everywhere instead of DOT. Dropped ComplexIndex.

--- fctools/ComplexConjugate.m
+++ fctools/ComplexConjugate.m
@@ -24,8 +24,7 @@ BeginPackage["HighEnergyPhysics`fctools`ComplexConjugate`",
 ComplexConjugate::"usage"=
 "ComplexConjugate[expr] complex conjugates expr.
 It operates on  Fermion-lines, i.e., products
-of Spinor[..] .DiracMatrix[..] . Spinor[..], and changes all
-occuring LorentzIndex[mu] into LorentzIndex[ComplexIndex[mu]].
+of Spinor[..] .DiracMatrix[..] . Spinor[..].
 For taking the spin sum (i.e. constructing the traces) use
 FermionSpinSum.\n\n
 WARNING: In expr should be NO explicit I in denominators!";
@@ -39,7 +38,6 @@ ccm                 = MakeContext["ChargeConjugationMatrix"];
 ccmi                = MakeContext["ChargeConjugationMatrixInv"];
 DiracGamma          = MakeContext["DiracGamma"];
 Expanding           = MakeContext["Expanding"];
-dot                 = MakeContext["DOT"];
 DotSimplify         = MakeContext["DotSimplify"];
 fci                 = MakeContext["FeynCalcInternal"];
 FreeQ2              = MakeContext["FreeQ2"];
@@ -47,7 +45,7 @@ Isolate             = MakeContext["Isolate"];
 IsolateNames         = MakeContext["IsolateNames"];
 IsolateSplit        = MakeContext["IsolateSplit"];
 LorentzIndex        = MakeContext["LorentzIndex"];
-ComplexIndex        = MakeContext["ComplexIndex"];
+(*ComplexIndex        = MakeContext["ComplexIndex"];*)
 Spinor              = MakeContext["Spinor"];
 SUND                = MakeContext["SUND"];
 SUNDelta            = MakeContext["SUNDelta"];
@@ -68,7 +66,7 @@ HoldPattern[ rev[yz__ /; FreeQ2[{yz}, {SUNT}] ] ]:=
 (* CHANGE 26/07/94 *)
 HoldPattern[ rev[yz__] ]:=
 Isolate[
-dot @@ (Reverse[FRH[{ yz }]]/.
+DOT @@ (Reverse[FRH[{ yz }]]/.
 (*Changed 28/2-2001 by F.Orellana, because of bug report by T.Rashba*)(*
                     DiracGamma[5]->(-DiracGamma[5])/.
                    {DiracGamma[6] :> DiracGamma[7],
@@ -79,33 +77,37 @@ dot @@ (Reverse[FRH[{ yz }]]/.
 
 c$CCfrh /: HoldForm[c$CCfrh[ii_]] := c$CC[ii];
 
-cLIndex[x_, dime___] := LorentzIndex[ComplexIndex[x], dime];
-cSIndex[x_]          := SUNIndex[ComplexIndex[x]];
+(*cLIndex[x_, dime___] := LorentzIndex[ComplexIndex[x], dime];
+cSIndex[x_]          := SUNIndex[ComplexIndex[x]];*)
 
 conpa[x__] := Pair[x] /. {Polarization[k_, a_, in___] :>
                           Polarization[k, Conjugate[a], in]};
 (* ComplexConjugatedef *)
-sunfcomp[a___] := SUNF @@ ({a}/.ComplexIndex -> Identity);
-sundeltacomp[a___] := SUNDelta @@ ({a}/.ComplexIndex -> Identity);
+(*sunfcomp[a___] := SUNF @@ ({a}/.ComplexIndex -> Identity);
+sundeltacomp[a___] := SUNDelta @@ ({a}/.ComplexIndex -> Identity);*)
 (*Added SumOver stuff. F.Orellana. 20/8-2002*)
-sumovercomp[a___] := HighEnergyPhysics`FeynArts`SumOver @@ ({a}/.ComplexIndex -> Identity);
+(*Dropped again 19/1-2003. Let's try and keep some hierarchy:
+  FeynCalc -> PHI -> FeynArts. Dropped this whole business and ComplexIndex also;
+  it is not maintainable (say you want another real function - you don't want to
+  have to to type it in here in this file...)*)
+(*sumovercomp[a___] := HighEnergyPhysics`FeynArts`SumOver @@ ({a}/.ComplexIndex -> Identity);
+ugencomp[a__] := HighEnergyPhysics`Phi`Objects`UGenerator @@ ({a}/.ComplexIndex -> Identity);*)
 
-ugencomp[a__] := HighEnergyPhysics`Phi`Objects`UGenerator @@ ({a}/.ComplexIndex -> Identity);
 (*
 nenenen
 sundcomp[a___] := SUND @@ ({a}/.ComplexIndex -> Identity);
 *)
 
 ComplexConjugate[b_HoldForm] := b /; FreeQ2[fci[FRH[b]],
-                                {dot,LorentzIndex,SUNIndex,Complex}];
+                                {DOT,LorentzIndex,SUNIndex,Complex}];
 
 ComplexConjugate[x_ /; (Head[x] =!= HoldForm)] :=
                  compcon[fci[x]/.SUNTrace->suntrac
-                        ] /. SUNF -> sunfcomp /.
-                    HighEnergyPhysics`FeynArts`SumOver -> sumovercomp /.
-                    HighEnergyPhysics`Phi`Objects`UGenerator -> ugencomp /.
+                        ] /. (*SUNF -> sunfcomp /.*)
+                    (*HighEnergyPhysics`FeynArts`SumOver -> sumovercomp /.
+                    HighEnergyPhysics`Phi`Objects`UGenerator -> ugencomp /.*)
                     SUNDelta -> SUNDeltaContract /.
-                       SUNDeltaContract -> sundeltacomp/.
+                       (*SUNDeltaContract -> sundeltacomp/.*)
                           compcon -> compcon2 /. compcon2 ->
                              ComplexConjugate /. suntrac->
                               SUNTrace;
@@ -116,7 +118,7 @@ compcon[x_Plus] := compcon /@ x;
 compcon[x_Times]:= compcon /@ x;
 
 compcon[b_HoldForm] := b /;
-             FreeQ2[FRH[b], {dot,LorentzIndex,SUNIndex,Complex}];
+             FreeQ2[FRH[b], {DOT,LorentzIndex,SUNIndex,Complex}];
 compcon[x_ /; (Head[x] =!= Plus) && (Head[x] =!= Times)] :=
              Block[{nx=x,oone, suntrac},
                   If[!FreeQ[nx, SUNF], nx = Expand[nx, SUNF]];
@@ -125,19 +127,19 @@ compcon[x_ /; (Head[x] =!= Plus) && (Head[x] =!= Times)] :=
                         nx = DotSimplify[nx, Expanding -> False];
                       ];
 (* this is wrong if nx had Head List ... (change 02/99)
-                    nx = (dot[oone, nx] /. dot -> rev /. rev -> dot);
+                    nx = (DOT[oone, nx] /. DOT -> rev /. rev -> DOT);
 *)
                     nx = nx /. DiracGamma[a__]:>
-                    dot[oone, DiracGamma[a]](*Added 28/2-2001 by F.Orellana - see above*)/;
+                    DOT[oone, DiracGamma[a]](*Added 28/2-2001 by F.Orellana - see above*)/;
                     FreeQ[{5,6,7},Evaluate[{a}[[1]]]]/.
                     DiracGamma[5]->(-DiracGamma[5])/.
                    {DiracGamma[6] :> DiracGamma[7],
                     DiracGamma[7]:>DiracGamma[6]}(**)/.
-                         dot -> rev /. rev -> dot;
+                         DOT -> rev /. rev -> DOT;
                     nx = nx //. c$CC -> c$CCfrh /. oone -> 1;
                     nx = nx /.
-                          LorentzIndex -> cLIndex /.
-                          SUNIndex  -> cSIndex /.
+                          (*LorentzIndex -> cLIndex /.
+                          SUNIndex  -> cSIndex /.*)
                           Complex[a_, b_] -> Complex[a, -b] ;
                     nx = dotlin[nx];
                 nx] /; FreeQ[x, HoldForm];
