e905c64515ef349671781cd0df0c404b4abc8469
Frederik Orellana <frederik@orellana.dk>
2003-04-06 18:05:10 +0000

    "subthreshold" is a more sensible setting of LeutwylerJBarEvaluation when taking limits.

--- Phi/PhiStart.m
+++ Phi/PhiStart.m
@@ -203,7 +203,7 @@ have a meson-field dependence.";
    SetOptions[ILimit, FunctionLimits -> {Log -> Log, 
    LeutwylerJBar -> (LeutwylerJBar[
    Sequence @@ Select[Expand /@ {##}, ((! MatchQ[#, _Rule | _List]) &)],
-   LeutwylerJBarEvaluation -> "physical",
+   LeutwylerJBarEvaluation -> "subthreshold",
    ExplicitLeutwylerSigma -> True]&)}];
    
    SetOptions[B0,
