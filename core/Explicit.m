723f3530b79b4afda5c8c4e3ff9a3bfd242cc0ad
Rolf Mertig <rolfm@gluonvision.com>
2003-09-29 15:28:33 +0000

    rewrite. needed for qcd examples

--- core/Explicit.m
+++ core/Explicit.m
@@ -21,39 +21,41 @@ by SUNTrace objects.";
 Begin["`Private`"];
    
 
-MakeContext[ExpandScalarProduct];
+MakeContext[ExpandScalarProduct,
+FieldStrength,
+GluonPropagator,
+GhostPropagator,
+QuarkPropagator,
+GluonVertex,
+GluonGhostVertex,
+GluonGhostVertex,
+QuarkGluonVertex
+];
 
 FieldStrength := FieldStrength = MakeContext["FieldStrength"];
 
-Explicit[y_] := Block[{gv, t2g, fi, pr, r = y}, 
-        If[
-           CheckContext["GluonVertex"]
-           ,
-           gv[x__]  := gv[x] = 
-           Expand[ExpandScalarProduct[
-           MakeContext["GluonVertex"][x, Explicit->True]]];
-           r = r /. MakeContext["GluonVertex"] -> gv
-          ];
-        If[
-           CheckContext["Twist2GluonOperator"],
-           t2g[x__] := t2g[x] = 
-           MakeContext["Twist2GluonOperator"][x, Explicit->True];
-           r = r /. MakeContext["Twist2GluonOperator"] -> t2g 
-          ];
-        If[
-           CheckContext["FieldStrength"]
-           ,
-           fi[x__] := fi[x] = 
-           MakeContext["FieldStrength"][x, Explicit->True];
-           r = r /. MakeContext["FieldStrength"] -> fi 
-          ];
-        If[
-           CheckContext["GluonPropagator"]
-           ,
-           pr[x__] := pr[x] =
-           MakeContext["GluonPropagator"][x, Explicit->True];
-           r = r /. MakeContext["GluonPropagator"] -> pr
-          ];
+Explicit[y_] := Block[{gh, gvv, gv, t2g, fis, pr, r = y}, 
+           gv[x__]  := gv[x]  = Expand[ExpandScalarProduct[ GluonVertex[x, Explicit -> True]]];
+           gp[x__]  := gp[x]  = Expand[ExpandScalarProduct[ GluonPropagator[x, Explicit -> True]]];
+           gh[x__]  := gh[x]  = Expand[ExpandScalarProduct[ GhostPropagator[x, Explicit -> True]]];
+           gvv[x__] := ghv[x] = Expand[ExpandScalarProduct[ GluonGhostVertex[x, Explicit -> True]]];
+           qgv[x__] := qgv[x] = Expand[ExpandScalarProduct[ QuarkGluonVertex[x, Explicit -> True]]];
+           r = r /. {GluonVertex :> gv, GluonGhostVertex :> gvv, GhostPropagator :> gh, 
+                     GluonPropagator :> gp, QuarkGluonVertex :> qgv
+                    };
+
+           If[CheckContext["Twist2GluonOperator"],
+              t2g[x__] := t2g[x] = MakeContext["Twist2GluonOperator"][x, Explicit->True]; 
+              r = r /. MakeContext["Twist2GluonOperator"] -> t2g ];
+
+           If[CheckContext["Twist2QuarkOperator"],
+              t2q[x__] := t2q[x] = MakeContext["Twist2QuarkOperator"][x, Explicit->True]; 
+              r = r /. MakeContext["Twist2QuarkOperator"] -> t2q ];
+
+           If[CheckContext["FieldStrength"],
+              fis[x__] := fis[x] = MakeContext["FieldStrength"][x, Explicit->True]; 
+              r = r /. MakeContext["FieldStrength"] -> fis];
+
                    r];
 
 End[]; EndPackage[];
