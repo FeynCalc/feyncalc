88d4c06b2e6525c5ce2f3bd1f484e3073ef5d85d
Rolf Mertig <rolfm@gluonvision.com>
2003-10-06 17:26:27 +0000

    Since it seems useful Coefficient2 got resurrected ...

---
+++ general/Coefficient2.m
@@ -0,0 +1,29 @@
+(* :Tite: Coefficient2 *)
+                                                                                                                                           
+BeginPackage["HighEnergyPhysics`general`Coefficient2`",
+             "HighEnergyPhysics`FeynCalc`"];
+                                                                                                                                           
+Coefficient2::usage=
+"Coefficient2
+fixes a bug in Coefficient (for V2.2) concerning non-integer powers and
+Coefficient2[a/x,x,0] returns 0.";
+                                                                                                                                           
+Begin["`Coefficient2`"];
+                                                                                                                                           
+Coefficient2[x_,y__] := Module[{pow},
+If[!FreeQ[x, _ ^ n_ /; Head[n]=!=Integer],
+   Operate[ReleaseHold,
+   Operate[Hold,
+   (Coefficient@@({x,y}/.w_ ^ n_ /; Head[n]=!=Integer :> pow[w,n])
+   )      ] /. pow->Power
+          ],
+   Coefficient[x,y]
+  ]
+  ];
+                                                                                                                                           
+End[]; 
+EndPackage[];
+(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+If[$VeryVerbose > 0,WriteString["stdout", "Coefficient2| \n "]];
+Null
+
