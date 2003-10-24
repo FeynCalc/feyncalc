adbe34879c911e263dbf718a298fb731dcbcd77b
Rolf Mertig <rolfm@gluonvision.com>
2003-10-24 15:50:59 +0000

    More or less simple but educational example calculations. The .out files are the  result of running them (on Linux).

---
+++ fcexamples/e+e-mu+mu-.m
@@ -0,0 +1,32 @@
+<<HighEnergyPhysics`FeynCalc`
+
+(* this example is the FeynCalc version of the example on page 56 from the Form Tutorial (2000) by Andre Heck *)
+
+starttime = AbsoluteTime[];
+
+{k1, k2, p1, p2} = DiracSlash /@ {K1, K2, P1, P2};
+{mu, nu, rho, sigma} = DiracMatrix /@ {MU, NU, RHO, SIGMA};
+
+SetMandelstam[s,t,u, P1, P2, -K1, -K2, 0, 0, 0, 0];
+
+M2 =  (* electron line *)
+      e^2 * TR[k1 . rho . k2 . sigma] *
+      (* photon propagator *)
+      MT[RHO , MU] MT[SIGMA, NU] / s^2 *
+      (* muon spin line *)
+      e^2 * TR[p1 . mu . p2 . nu];
+
+(* this does the contraction *)
+r = Contract[M2];
+
+(* this is equivalent to the 
+Bracket e,s  
+statement of the FORM example
+*) 
+Print["the result is: ", res = Collect3[r, {e, s}]];
+
+Print["calculation time used  = ", AbsoluteTime[]-starttime]
+
+
+
+
