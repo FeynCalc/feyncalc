be63ccb3c14e074acc51a40570f3654433031bea
Rolf Mertig <rolfm@gluonvision.com>
2003-11-09 11:55:28 +0000

    Factor changed to Factoring

--- general/Collect3.m
+++ general/Collect3.m
@@ -15,7 +15,7 @@ Collect3::"usage"=
 "Collect3[expr, head] collects all monoomials of the form head[..]*head[..]*..
  in expr. \n
 Collect3[expr, {x, y, ...}] collects terms involving the same powers
-of monomials x^n1*y^n2 ... An option Factor -> True/False can be 
+of monomials x[...]^n1*y[...]^n2 ... An option Factoring -> True/False/Factor2 can be 
 given, which factors the coefficients. The option Head (default Plus)
 determines the applied function to the list of monomials 
 mulitplied by their coefficients.";
@@ -23,17 +23,30 @@ mulitplied by their coefficients.";
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Private`"];
+
+MakeContext[Factoring];
    
-Options[Collect3] = {Factor -> False, Head -> Plus};
+Options[Collect3] = {Factoring -> False, Head -> Plus};
 
 Collect3[expr_, vars_/;Head[vars]=!=List, opts___Rule] :=
  Collect3[expr, Cases[expr, HoldPattern[vars[___]], -1], opts];
 
-Collect3[expr_, vars_List, opts___Rule] :=
-     Apply[Head/.{opts}/.Options[Collect3],
-           If[Factor/.{opts}/.Options[Collect3],Map[Factor, #], #]& @
-           MonomialList[expr, vars, CoefficientDomain->RationalFunctions]
-          ];
+Collect3[expr_, vars_List, opts___?OptionQ] := Block[{fac, hva, mli},
+     fac = Factoring/.{opts}/.Options[Collect3];
+     If[fac === True, fac = Factor];
+(* remember that in FeynCalc.m there is:
+    System`MonomialList =
+    Internal`FromDistributedTermsList[
+      Internal`DistributedTermsList[#1, Sequence@@Drop[{##}, 1]],
+    List]&,
+*)
+     hva = (Hold[HoldPattern][#[___]]& /@ ( Hold/@vars ) ) /. Hold[a_] :> a;
+     hva = Alternatives @@ hva;
+     mli = MonomialList[expr, Union@Cases[expr,hva,-1], 
+                        CoefficientDomain -> RationalFunctions ];
+     If[fac =!= False, mli = Map[fac, mli]];
+     Apply[Head/.{opts}/.Options[Collect3], mli]
+];
 
 End[]; EndPackage[];
 (* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
