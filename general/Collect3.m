0c0a06d743db0583018bb9ce832cbbf05f3da671
Rolf Mertig <rolfm@gluonvision.com>
2003-11-10 19:50:11 +0000

    better compatibility with Collect2

--- general/Collect3.m
+++ general/Collect3.m
@@ -28,8 +28,13 @@ MakeContext[Factoring];
    
 Options[Collect3] = {Factoring -> False, Head -> Plus};
 
+Collect3[a_,b_,c_Symbol, opts___?OptionQ]:= Collect3[a, b, Factoring -> c, opts];
+
 Collect3[expr_, vars_/;Head[vars]=!=List, opts___Rule] :=
+ Collect3[expr, {vars}, opts];
+(*
  Collect3[expr, Cases[expr, HoldPattern[vars[___]], -1], opts];
+*)
 
 Collect3[expr_, vars_List, opts___?OptionQ] := Block[{fac, hva, mli},
      fac = Factoring/.{opts}/.Options[Collect3];
@@ -42,9 +47,7 @@ Collect3[expr_, vars_List, opts___?OptionQ] := Block[{fac, hva, mli},
 *)
      hva = (Hold[HoldPattern][#[___]]& /@ ( Hold/@vars ) ) /. Hold[a_] :> a;
      hva = Alternatives @@ hva;
-     mli = MonomialList[expr, Union@Cases[expr,hva,-1], 
-                        CoefficientDomain -> RationalFunctions ];
-     If[fac =!= False, mli = Map[fac, mli]];
+     mli = MonomialList[expr, Union@Cases[expr,hva,-1], CoefficientDomain -> RationalFunctions ]; If[fac =!= False, mli = Map[fac, mli]];
      Apply[Head/.{opts}/.Options[Collect3], mli]
 ];
 
