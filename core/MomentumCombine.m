3cd40b2c234d0c2c23569c94897e5d3857fc81cb
Rolf Mertig <rolfm@gluonvision.com>
2003-11-14 18:03:18 +0000

    optimized this for Pair

--- core/MomentumCombine.m
+++ core/MomentumCombine.m
@@ -9,30 +9,35 @@ BeginPackage["HighEnergyPhysics`FeynCalc`MomentumCombine`",
              "HighEnergyPhysics`FeynCalc`"];
 
 MomentumCombine::"usage" =
-"MomentumCombine[expr]  is the inverse operation to
-MomentumExpand and ExpandScalarProduct.
-MomentumCombine combines also Pair`s.";
+"MomentumCombine[expr] combines momenta and Pairs with the same 
+Lorentz indices and momenta.";
 
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Private`"];
 
 MakeContext[ FeynCalcInternal];
+lor := lor = MakeContext["LorentzIndex"];
 momentum := momentum = MakeContext["Momentum"];
 pair   := pair = MakeContext["Pair"];
 
 (*momentumExpanddef*)
 
-MomentumCombine[expr_] :=
+Options[MomentumCombine] = {LeafCount -> 1000};
+
+MomentumCombine[expr_, opts___?OptionQ] :=
+If[LeafCount[expr] < (LeafCount /. {opts} /. Options[MomentumCombine]),
+   expr,
 If[FreeQ[expr, momentum], FeynCalcInternal[expr], expr] //. {
  (n3_. momentum[x_,di___] + n4_. momentum[y_,di___]
  ):>
  (momentum[ Expand[n3 x + n4 y],di]/;(NumberQ[n3]&&NumberQ[n4])),
- (n3_. pair[a_, momentum[x_,di___]] + n4_. pair[a_, momentum[y_,di___]]
+ (n3_. pair[a_lor, momentum[x_,di___]] + n4_. pair[a_lor, momentum[y_,di___]]
  ):>
  (pair[a, momentum[ Expand[n3 x + n4 y],di]
       ]/;(NumberQ[n3] && NumberQ[n4]))
-                             };
+                             }
+];
 End[]; EndPackage[];
 (* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
 If[$VeryVerbose > 0,WriteString["stdout", "MomentumCombine | \n "]];
