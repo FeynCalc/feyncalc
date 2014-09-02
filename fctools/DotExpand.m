bd9a16372cbee44aaa8e9dfbbdf26cceb07c27f7
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2014-09-02 15:03:21 +0000

    DotExpand can now split spinor chains (like spinlin in DiracSimplify)

--- fctools/DotExpand.m
+++ fctools/DotExpand.m
@@ -23,13 +23,14 @@ DotExpand::"usage" =
 
 Begin["`Private`"];
 
-MakeContext[(*DOT, *)NonCommFreeQ, NonCommQ];
+MakeContext[(*DOT, *)NonCommFreeQ, NonCommQ, Spinor];
 
 DotExpand[expr_] :=
     expr //. {DOT[a___, b_ + c_, d___] :> Distribute[DOT[a, b + c, d]],
   DOT[a___, b_*c_, d___] :> b*DOT[a, c, d] /; NonCommFreeQ[b],
   DOT[a___, b_, d___] :> b*DOT[a, d] /; NonCommFreeQ[b],
-  DOT[a___, b_*c__, d___] :> DOT[a, b, c, d] /; NonCommQ[b]} /.
+  DOT[a___, b_*c__, d___] :> DOT[a, b, c, d] /; NonCommQ[b],
+  DOT[a_Spinor,b___,c_Spinor,d_Spinor,e___,f_Spinor,g___]:> DOT[a,b,c] DOT[d,e,f,g]} /.
   DOT[] :> Sequence[];
 
 End[]; EndPackage[];
