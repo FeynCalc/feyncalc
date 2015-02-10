c09737b9fba17c17f7a3c39d747292847d05a947
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-02-10 15:23:34 +0100

    Updated FAPatch and FCPrepareFAAmp. The latter is now capable of semi-automatic conversions from FeynArts to FeynCalc.

--- FeynCalc/fctools/FCPrepareFAAmp.m
+++ FeynCalc/fctools/FCPrepareFAAmp.m
@@ -11,55 +11,85 @@
    Copyright (C) 2014-2015 Vladyslav Shtabovenko
 *)
 
-(* :Summary:  FCPrepareFAAmp replaces FA* objects by the corresponding
-                FeynCalc objects                                            *)
+(* :Summary:  FCPrepareFAAmp converts a FeynArts amplitude to FeynCalc      *)
 
 (* ------------------------------------------------------------------------ *)
 
 BeginPackage["HighEnergyPhysics`fctools`FCPrepareFAAmp`",{"HighEnergyPhysics`FeynCalc`"}];
 
 FCPrepareFAAmp::"usage" =
-"FCPrepareFAAmp[exp] replaces objects like FASlash, FADiracMatrix etc., \n
-by the corresponding FeynCalc objects.";
+"FCPrepareFAAmp[exp] converts a FeynArts amplitude to FeynCalc.";
+
+UndoChiralSplittings::"usage" =
+"UndoChiralSplittings is an option of FCPrepareFAAmp. When set to True, it attempts
+to undo splittings of couplings into left and right handed pieces, e.g
+(a*GA[6].GA[mu] + a*GA[7].GA[mu]) will be converted back to a*GA[mu]";
 
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Private`"];
 
-ChiralityProjector = MakeContext["CoreObjects","ChiralityProjector"];
-DiracMatrix = MakeContext["CoreObjects","DiracMatrix"];
-DiracSlash = MakeContext["CoreObjects","DiracSlash"];
-DiracSpinor = MakeContext["CoreObjects","DiracSpinor"];
-FeynAmp = MakeContext["CoreObjects","FeynAmp"];
-FeynAmpDenominator = MakeContext["CoreObjects","FeynAmpDenominator"];
-FourVector = MakeContext["CoreObjects","FourVector"];
-Gstrong = MakeContext["CoreObjects","Gstrong"];
-GaugeXi = MakeContext["CoreObjects","GaugeXi"];
-LorentzIndex = MakeContext["CoreObjects","LorentzIndex"];
-MetricTensor = MakeContext["CoreObjects","MetricTensor"];
-NonCommutative = MakeContext["CoreObjects","NonCommutative"];
-PolarizationVector = MakeContext["CoreObjects","PolarizationVector"];
-PropagatorDenominator = MakeContext["CoreObjects","PropagatorDenominator"];
+DiracGamma              := DiracGamma               = MakeContext["CoreObjects","DiracGamma"];
+FeynAmp                 := FeynAmp                  = MakeContext["CoreObjects","FeynAmp"];
+FeynAmpDenominator      := FeynAmpDenominator       = MakeContext["CoreObjects","FeynAmpDenominator"];
+GaugeXi                 := GaugeXi                  = MakeContext["CoreObjects","GaugeXi"];
+Gstrong                 := Gstrong                  = MakeContext["CoreObjects","Gstrong"];
+LorentzIndex            := LorentzIndex             = MakeContext["CoreObjects","LorentzIndex"];
+Momentum                := Momentum                 = MakeContext["CoreObjects","Momentum"];
+NonCommutative          := NonCommutative           = MakeContext["CoreObjects","NonCommutative"];
+Pair                    := Pair                     = MakeContext["CoreObjects","Pair"];
+Polarization            := Polarization             = MakeContext["CoreObjects","Polarization"];
+PropagatorDenominator   := PropagatorDenominator    = MakeContext["CoreObjects","PropagatorDenominator"];
+SUNF                    := SUNF                     = MakeContext["CoreObjects","SUNF"];
+SUNFIndex               := SUNFIndex                = MakeContext["CoreObjects","SUNFIndex"];
+SUNIndex                := SUNIndex                 = MakeContext["CoreObjects","SUNIndex"];
+SUNTF                   := SUNTF                    = MakeContext["CoreObjects","SUNTF"];
+Spinor                  := Spinor                   = MakeContext["CoreObjects","Spinor"];
 
 MakeContext[DiracTrace];
 
-FCPrepareFAAmp[expr_] :=
-    ReplaceAll[expr,{
-    Global`FAFourVector -> FourVector,
-    Global`FAPolarizationVector -> PolarizationVector,
-    Global`FADiracSpinor -> DiracSpinor,
-    Global`FADiracSlash -> DiracSlash,
-    Global`FADiracMatrix -> DiracMatrix,
-    Global`FADiracTrace -> DiracTrace,
-    Global`FAMetricTensor -> MetricTensor,
-    Global`FAChiralityProjector ->ChiralityProjector,
-    Global`FAGS -> Gstrong,
-    HighEnergyPhysics`FeynArts`FAGaugeXi -> GaugeXi,
-    HighEnergyPhysics`FeynArts`FANonCommutative -> NonCommutative,
-    HighEnergyPhysics`FeynArts`FAFeynAmp -> FeynAmp,
-    HighEnergyPhysics`FeynArts`FAFeynAmpDenominator -> FeynAmpDenominator,
-    HighEnergyPhysics`FeynArts`FAPropagatorDenominator -> PropagatorDenominator
-    }];
+Options[FCPrepareFAAmp] = {UndoChiralSplittings -> False};
+
+FCPrepareFAAmp[expr_, OptionsPattern[]] :=
+    Block[ {replist1,replist2,tempvar,temp},
+        replist1 = {HighEnergyPhysics`FeynArts`Index[Global`Lorentz, x_] :> LorentzIndex[ToExpression["Lor" <> ToString[x]]],
+                    HighEnergyPhysics`FeynArts`Index[Global`Gluon, x_] :> SUNIndex[ToExpression["Glu" <> ToString[x]]],
+                    HighEnergyPhysics`FeynArts`Index[Global`Colour, x_] :> SUNFIndex[ToExpression["Col" <> ToString[x]]],
+                    HighEnergyPhysics`FeynArts`FourMomentum[HighEnergyPhysics`FeynArts`Incoming,x_] :> ToExpression["InMom" <> ToString[x]],
+                    HighEnergyPhysics`FeynArts`FourMomentum[HighEnergyPhysics`FeynArts`Outgoing,x_] :> ToExpression["OutMom" <> ToString[x]],
+                    HighEnergyPhysics`FeynArts`FourMomentum[HighEnergyPhysics`FeynArts`Internal,x_] :> ToExpression["LoopMom" <> ToString[x]]
+                    };
+        replist2 = Dispatch[{
+                    Conjugate[Global`FAPolarizationVector][_, x_, y_] :> Pair[LorentzIndex[y],Momentum[Polarization[x,-I]]],
+                    Global`FAChiralityProjector[-1] :> DiracGamma[7],
+                    Global`FAChiralityProjector[1] :> DiracGamma[6],
+                    Global`FADiracMatrix :> DiracGamma,
+                    Global`FADiracSlash[x_] :> DiracGamma[Momentum[x]],
+                    Global`FADiracSpinor :> Spinor,
+                    Global`FADiracTrace :> DiracTrace,
+                    Global`FAFourVector[x_,y_] :> Pair[Momentum[x],y],
+                    Global`FAGS :> Gstrong,
+                    Global`FAMetricTensor :> Pair,
+                    Global`FAPolarizationVector[_, x_, y_] :> Pair[LorentzIndex[y],Momentum[Polarization[x,I]]],
+                    Global`FASUNF[a_,b_,c_, d_] :> (Clear[tempvar];
+                                                    tempvar = Unique[$AL];
+                                                    SUNF[a,b,tempvar]SUNF[tempvar,c,d]),
+                    Global`FASUNF[a_,b_,c_] :> SUNF[a,b,c],
+                    Global`FASUNT :> SUNTF,
+                    HighEnergyPhysics`FeynArts`FAFeynAmp :> FeynAmp,
+                    HighEnergyPhysics`FeynArts`FAFeynAmpDenominator :> FeynAmpDenominator,
+                    HighEnergyPhysics`FeynArts`FAGaugeXi :> GaugeXi,
+                    HighEnergyPhysics`FeynArts`FANonCommutative :> DOT,
+                    HighEnergyPhysics`FeynArts`FAPropagatorDenominator :> PropagatorDenominator,
+                    HighEnergyPhysics`FeynArts`FermionChain :> DOT,
+                    HighEnergyPhysics`FeynArts`MatrixTrace :> DiracTrace
+                    }];
+        temp = expr /. replist1 /. replist2;
+        If[ OptionValue[UndoChiralSplittings],
+            temp = temp/.{(a1__ DiracGamma[x_].DiracGamma[6] a2__ + a1__ DiracGamma[x_].DiracGamma[7] a2__) :> a1 DiracGamma[x] a2}
+        ];
+        temp
+    ];
 
 End[];
 EndPackage[];
