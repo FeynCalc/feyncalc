75bc754ae9e8b6430dea0f1d2362eb4442775607
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-03-06 21:30:26 +0100

    Added tests for verifying tensor decompositions in TIDL via Tdec.

---
+++ FeynCalc/fcexamples/VerifyTIDLFourLoop.m
@@ -0,0 +1,36 @@
+(* ::Package:: *)
+
+(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+
+(* :Title: VerifyTIDLFourLoop												*)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:	Verify 4-loop tensor decomposition formulas stored in TIDL
+				using Tdec. Warning: The computations can take quite
+				some time...												*)
+
+(* ------------------------------------------------------------------------ *)
+
+
+<<HighEnergyPhysics`FeynCalc`;
+
+On[Assert];
+
+checkFormula[ex_] :=
+	Assert[Simplify[Tdec[Sequence @@ ex, UseTIDL -> False, List -> False,
+			FeynCalcExternal -> False] - TIDL[Sequence @@ ex]] === 0];
+
+Print["Checking three-loop tensor decompositions"];
+
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 d^D q3 q1^mu q2^nu q3^rho q4^sigma f(q1,q2,q3,q4)"];
+ex = {{{q1, mu},{q2, nu},{q3, rho},{q4, sigma}}, {}};
+checkFormula[ex]
+]
---
+++ FeynCalc/fcexamples/VerifyTIDLTwoLoop.m
@@ -0,0 +1,98 @@
+(* ::Package:: *)
+
+(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+
+(* :Title: VerifyTIDLTwoLoop												*)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:	Verify 2-loop tensor decomposition formulas stored in TIDL
+				using Tdec. Warning: The computations can take quite
+				some time...												*)
+
+(* ------------------------------------------------------------------------ *)
+
+
+<<HighEnergyPhysics`FeynCalc`;
+
+On[Assert];
+
+checkFormula[ex_] :=
+	Assert[Simplify[Tdec[Sequence @@ ex, UseTIDL -> False, List -> False,
+			FeynCalcExternal -> False] - TIDL[Sequence @@ ex]] === 0];
+
+Print["Checking two-loop tensor decompositions"];
+
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q2^nu f(q1,q2)"];
+ex = {{{q1, mu},{q2, nu}}, {}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q2^nu f(q1,q2,p1)"];
+ex = {{{q1, mu},{q2, nu}}, {p1}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q2^nu f(q1,q2,p1,p2)"];
+ex = {{{q1, mu},{q2, nu}}, {p1,p2}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q2^nu f(q1,q2,p1,p2,p3)"];
+ex = {{{q1, mu},{q2, nu}}, {p1,p2,p3}};
+checkFormula[ex]
+]
+
+(*	3-4 minutes on a Core i7	*)
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q2^nu f(q1,q2,p1,p2,p3,p4)"];
+ex = {{{q1, mu},{q2, nu}}, {p1,p2,p3,p4}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q2^rho f(q1,q2,p1)"];
+ex = {{{q1, mu},{q1, nu},{q2,rho}}, {p1}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q2^rho f(q1,q2,p1,p2)"];
+ex = {{{q1, mu},{q1, nu},{q2,rho}}, {p1,p2}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q2^rho f(q1,q2,p1,p2,p3)"];
+ex = {{{q1, mu},{q1, nu},{q2,rho}}, {p1,p2,p3}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q1^rho q2^si  f(q1,q2)"];
+ex = {{{q1, mu},{q1, nu},{q1,rho},{q2,si}}, {}};
+checkFormula[ex]
+]
+
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q1^rho q2^si  f(q1,q2,p1)"];
+ex = {{{q1, mu},{q1, nu},{q1,rho},{q2,si}}, {p1}};
+checkFormula[ex]
+]
+
+(*	30 - 40 seconds on a Core i7	*)
+Print @ AbsoluteTiming[
+Print ["d^D q1 d^D q2 q1^mu q1^nu q1^rho q2^si  f(q1,q2,p1,p2)"];
+ex = {{{q1, mu},{q1, nu},{q1,rho},{q2,si}}, {p1,p2}};
+checkFormula[ex]
+]
