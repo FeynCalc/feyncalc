300589385e576a6e4a5589d11a9a61219bfb1a54
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-04-23 00:04:56 +0200

    Improved DiracEquation to properly apply equations of motion in case of spinors that depend on D-dimensional momenta  (CDR sheme)

--- FeynCalc/Dirac/DiracEquation.m
+++ FeynCalc/Dirac/DiracEquation.m
@@ -1,16 +1,19 @@
+(* ::Package:: *)
 
 
-(* :Title: DiracEquation *)
 
-(* :Author: Rolf Mertig *)
+(* :Title: DiracEquation													*)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Applies Dirac equation to simplify spinor chains				*)
 
 (* ------------------------------------------------------------------------ *)
-
-(* :Summary: Dirac equation application; not fully *)
-
-(* ------------------------------------------------------------------------ *)
-
-
 
 
 DiracEquation::usage =
@@ -24,14 +27,16 @@ End[]
 
 Begin["`DiracEquation`Private`"]
 
+HoldDOT;
+
 scev[a__] :=
 	scev[a] = ExpandScalarProduct[a];
 
-DiracEquation[x_] :=(*DiracEquation[x]=*)
+DiracEquation[x_] := (*DiracEquation[x]=*)
 	DotSimplify[diraceq[x//FeynCalcInternal], Expanding -> False];
 
 (* for only internal use *)
-DiracEquation[x_,I] :=(*DiracEquation[x]=*)
+DiracEquation[x_,I] := (*DiracEquation[x]=*)
 	DotSimplify[diraceq[x], Expanding -> False];
 
 last[_. Momentum[pe__]] :=
@@ -41,86 +46,76 @@ last[x_Plus] :=
 diraceq[x_] :=
 	x/;FreeQ[x,Spinor];
 diraceq[x_] :=
-	Expand[ x//.spCDieqRules, DOT ];
+	Expand[ x/. dirac67back /. DOT->HoldDOT //.spCDieqRules /.HoldDOT->DOT, DOT ];
+
+dirac67back = {1/2+DiracGamma[5]/2:>DiracGamma[6],1/2-DiracGamma[5]/2:>DiracGamma[7]};
+
+HoldDOT[a___,HoldDOT[b___],c___]:= HoldDOT[a,b,c];
+HoldDOT[a___,b1_HoldDOT + b2_HoldDOT + b3_:0 ,c___]:=
+	HoldDOT[a,b1,c]+HoldDOT[a,b2+b3,c];
 
 spCDieqRules = {
-	doot_[ z___,Spinor[n_. Momentum[p_] + k_. ,m_, op___], DiracGamma[Momentum[p_,___],___],a___] :>
-		(m/n doot[ z,Spinor[n Momentum[p] + k,m,op ],a] -
+	(* Now that we can use equations of motion only if all involved objects are in the same dimension!!! *)
+	HoldDOT[ z___,Spinor[n_. Momentum[p_, dim_ : 4] + k_:0 ,m_, op___], DiracGamma[Momentum[p_, dim_ : 4],dim_ : 4],a___] :>
+		(m/n HoldDOT[ z,Spinor[n Momentum[p, dim] + k,m,op ],a] -
 		If[ (k===0),
 			0,
-			If[ last[n Momentum[p] + k] =!= Momentum[p],
+			If[ last[n Momentum[p,dim] + k] =!= Momentum[p,dim],
 				0,
-				1/n doot[ z, Spinor[n Momentum[p] + k,m,op], DiracGamma[k],a]
+				1/n HoldDOT[ z, Spinor[n Momentum[p,dim] + k,m,op], DiracGamma[k,dim],a]
 			]
-		])/; last[n Momentum[p]+k]===Momentum[p],
-
-	doot_[ a___,DiracGamma[Momentum[p_,___],___], Spinor[n_. Momentum[p_] + k_. ,m_,op___],z___] :>
-		(m/n doot[ a,Spinor[ n Momentum[p] + k,m,op ],z] -
+		])/; last[n Momentum[p,dim]+k]===Momentum[p,dim],
+	HoldDOT[ a___,DiracGamma[Momentum[p_, dim_ : 4],dim_ : 4], Spinor[n_. Momentum[p_, dim_ : 4] + k_. ,m_,op___],z___] :>
+		(m/n HoldDOT[ a,Spinor[ n Momentum[p, dim] + k,m,op ],z] -
 		If[ (k===0),
 			0,
-			If[ last[n Momentum[p] + k] =!= Momentum[p],
+			If[ last[n Momentum[p,dim] + k] =!= Momentum[p,dim],
 				0,
-				1/n doot[ a, DiracGamma[k], Spinor[n Momentum[p] + k,m,op ], z]
+				1/n HoldDOT[ a, DiracGamma[k,dim], Spinor[n Momentum[p,dim] + k,m,op ], z]
 			]
 		]) /; last[n Momentum[p]+k]===Momentum[p],
 
-	doot_[ a___,DiracGamma[Momentum[y__],___], DiracGamma[Momentum[y__],___],b___] :>
-		scev[Momentum[y],Momentum[y]] doot[a,b],
+	HoldDOT[ a___,DiracGamma[Momentum[y_,dim_:4],dim_:4], DiracGamma[Momentum[y_,dim_:4],dim_:4],b___] :>
+		scev[Momentum[y,dim],Momentum[y,dim]] HoldDOT[a,b],
 
-	doot_[ z___,Spinor[n_. Momentum[p_] + k_. ,m_,op___],a___, DiracGamma[x_[y__],di___],
-	DiracGamma[Momentum[p_,dim___],dim___],b___] :>
-		If[ !FreeQ2[{a}, {DiracGamma[5], DiracGamma[6], DiracGamma[7]}],
-			DiracTrick[DOT[z,Spinor[n Momentum[p]+k,m,op],
-			(* fixed bug indicated by Peter Stoffer *)
-			a, DiracGamma[x[y],di],    DiracGamma[Momentum[p,dim],dim],b]] /. DOT -> doot,
-			(-doot[ z,Spinor[n Momentum[p]+k,m,op ],a, DiracGamma[Momentum[p,dim],dim],
-			DiracGamma[x[y],di],b] + 2(( PairContract[x[y],Momentum[p,dim] ]*
-			doot[ z,Spinor[n Momentum[p]+k,m,op],a,b]) /. PairContract -> Pair))
-		] /; last[n Momentum[p]+k] === Momentum[p],
+(* 	Here the situation is more complicated since we need to move the slash through a certain number
+	of other Dirac matrices before we reach the spinor. Note that the matrices
+	in the chain may have different dimensions.	*)
 
-	doot_[ a___,DiracGamma[Momentum[p_,___],___],DiracGamma[5],
-	Spinor[n_. Momentum[p_] + k_. ,m_,op___],z___] :>
-		(-m/n doot[a,DiracGamma[5],Spinor[n Momentum[p]+k,m,op],z]-
-		If[ k===0,
-			0,
-			If[ last[n Momentum[p] + k] =!= Momentum[p],
-				0,
-				1/n doot[ a, DiracGamma[k], DiracGamma[5],
-				Spinor[n Momentum[p] + k,m,op ], z]
-			]
-		])/; last[n Momentum[p]+k]===Momentum[p],
+	(* reaching the first spinor, if we need to anticommute past another Dirac gamma (not gamma 5)	*)
+	HoldDOT[	z___,	s: Spinor[n_. Momentum[p_, dim_ : 4] + k_. ,___],
+					a : DiracGamma[(LorentzIndex | ExplicitLorentzIndex | Momentum)[_, _ :4] | 5 | 6 | 7, _ : 4] ...,
+					DiracGamma[(x : LorentzIndex | ExplicitLorentzIndex | Momentum)[y_,di_:4],di_:4],
+					DiracGamma[Momentum[p_,dim_ : 4],dim_ : 4],b___] :>
+					-HoldDOT[ z,s,a, DiracGamma[Momentum[p,dim],dim],DiracGamma[x[y,di],di],b] +
+					2(PairContract[x[y,di],Momentum[p,dim]] /. PairContract -> Pair)*HoldDOT[ z,s,a,b]/;
+					last[n Momentum[p,dim]+k] === Momentum[p,dim],
 
-	doot_[ a___,DiracGamma[Momentum[p_,___],___],DiracGamma[6],
-	Spinor[n_. Momentum[p_] + k_. ,m_,op___],z___] :>
-		(m/n doot[a,DiracGamma[7],Spinor[n Momentum[p]+k,m,op],z]-
-		If[ k===0,
-			0,
-			If[ last[n Momentum[p] + k] =!= Momentum[p],
-				0,
-				1/n doot[ a, DiracGamma[k], DiracGamma[6],
-				Spinor[n Momentum[p] + k,m,op ], z]
-			]
-		])/; last[n Momentum[p]+k]===Momentum[p],
+	(* reaching the first spinor, if we need to anticommute past a Dirac gamma 5	*)
+	HoldDOT[	z___,	s: Spinor[n_. Momentum[p_, dim_ : 4] + k_. ,___],
+					a : DiracGamma[(LorentzIndex | ExplicitLorentzIndex | Momentum)[_, _ :4] | 5 | 6 | 7, _ : 4] ...,
+					DiracGamma[(u:5|6|7)],
+					DiracGamma[Momentum[p_,dim_ : 4],dim_ : 4],b___] :>
+					HoldDOT[ z,s,a, Anti5[DiracGamma[u].DiracGamma[Momentum[p,dim],dim]]/.DOT|Times->HoldDOT,b]/;
+					last[n Momentum[p,dim]+k] === Momentum[p,dim],
 
-	doot_[ a___,DiracGamma[Momentum[p_,___],___],DiracGamma[7],
-	Spinor[n_. Momentum[p_] + k_. ,m_,op___],z___] :>
-		(m/n doot[a,DiracGamma[6],Spinor[n Momentum[p]+k,m,op],z ]-
-		If[ k===0,
-			0,
-			If[ last[n Momentum[p] + k] =!= Momentum[p],
-				0,
-				1/n doot[ a, DiracGamma[k], DiracGamma[7],
-				Spinor[n Momentum[p] + k,m,op ], z]
-			]
-		])/; last[n Momentum[p]+k]===Momentum[p],
+	(* reaching the last spinor, if we need to anticommute past another Dirac gamma (not gamma 5)	*)
+	HoldDOT[	a___,	DiracGamma[Momentum[p_,dim_:4],dim_:4],
+					DiracGamma[(x : LorentzIndex | ExplicitLorentzIndex | Momentum)[y_,di_:4],di_:4],
+					b : DiracGamma[(LorentzIndex | ExplicitLorentzIndex | Momentum)[_, _ :4] | 5 | 6 | 7, _ : 4] ...,
+					s: Spinor[n_. Momentum[p_, dim_: 4] + k_.,___],z___] :>
+						(-HoldDOT[a,DiracGamma[x[y,di],di],DiracGamma[Momentum[p,dim],dim],b,s,z]+
+						2(PairContract[x[y,di],Momentum[p,dim]] /. PairContract -> Pair)*HoldDOT[a,b,s,z])/;
+						last[n Momentum[p,dim]+k]===Momentum[p,dim],
 
-	doot_[ a___,DiracGamma[ Momentum[p_,dim___],dim___],
-	DiracGamma[x_[y__],di___],b___,    Spinor[n_. Momentum[p_] + k_. ,m_,op___],z___] :>
-		(- doot[ a,DiracGamma[x[y],di],    DiracGamma[Momentum[p,dim],dim],b,
-		Spinor[n Momentum[p] + k,m,op ],z]+ 2(( PairContract[x[y],Momentum[p,dim]] *
-		doot[ a,b,Spinor[n Momentum[p] +k,m,op],z ])/. PairContract -> Pair)) /;
-		last[n Momentum[p]+k]===Momentum[p]
-	};
+	(* reaching the last spinor, if we need to anticommute past a Dirac gamma 5	*)
+	HoldDOT[	a___,	DiracGamma[Momentum[p_,dim_:4],dim_:4],
+					DiracGamma[(u:5|6|7)],
+					b : DiracGamma[(LorentzIndex | ExplicitLorentzIndex | Momentum)[_, _ :4] | 5 | 6 | 7, _ : 4] ...,
+					s: Spinor[n_. Momentum[p_, dim_: 4] + k_.,___],z___] :>
+						HoldDOT[a, Anti5[DiracGamma[Momentum[p,dim],dim].DiracGamma[u],-1]/.DOT|Times->HoldDOT,b,s,z]/;
+						last[n Momentum[p,dim]+k] === Momentum[p,dim]
+};
 
 FCPrint[1,"DiracEquation.m loaded."];
 End[]
--- Tests/Dirac/Dirac.mt
+++ Tests/Dirac/Dirac.mt
@@ -27,6 +27,11 @@ $BreitMaison = False;
 Map[Test[ToExpression[(#[[2]])],ToExpression[(#[[3]])],TestID->#[[1]]]&,
 	Join@@(ToExpression/@Names["Tests`Dirac`fcstChisholm*"])];
 
+$BreitMaison = False;
+Map[Test[ToExpression[(#[[2]])],ToExpression[(#[[3]])],TestID->#[[1]]]&,
+	Join@@(ToExpression/@Names["Tests`Dirac`fcstDiracEquation*"])];
+$BreitMaison = False;
+
 Map[Test[ToExpression[(#[[2]])],ToExpression[(#[[3]])],TestID->#[[1]]]&,
 	Join@@(ToExpression/@Names["Tests`Dirac`fcstDiracOrder*"])];
 
