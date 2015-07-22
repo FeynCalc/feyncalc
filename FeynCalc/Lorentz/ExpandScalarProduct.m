5255a7faa1f33db7eeaa18c07d90a5dc974e23f7
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-07-22 22:19:50 +0200

    Refactored ExpandScalarProduct.

--- FeynCalc/Lorentz/ExpandScalarProduct.m
+++ FeynCalc/Lorentz/ExpandScalarProduct.m
@@ -33,75 +33,64 @@ Options[ExpandScalarProduct] = {FCI -> True};
 (* well, not used, so commented out. F.Orellana, 8/11-2002. *)
 (*FRH = FixedPoint[ReleaseHold, #]&;*)
 
-ExpandScalarProduct[x_, op___?OptionQ] :=
+ExpandScalarProduct[x_, OptionsPattern[]] :=
 	Block[ {nx = x, pali},
-	(* RM: put the next line in front 20110408 *)
-		If[ (FCI /. {op} /. Options[ExpandScalarProduct]),
+
+		If[ OptionValue[FCI],
 			nx = FCI[nx]
 		];
+
 		If[ FreeQ[nx,Pair],
-			nx,
-(* this algorithm is much quicher on bigger expressions, maybe there should be a
-	switch for smaller ones to not use this?
-	Changed Nov 2003, RM
- *)
-			pali = Select[Cases2[nx, Pair], !FreeQ[#, LorentzIndex|Momentum]&];
-		(* Print["pali = ", pali//InputForm]; *)
-			If[ pali =!= {},
-				nx = nx /. Dispatch[Thread[pali -> oldExpandScalarProduct[pali]]]
-			];
-			nx
-		]
+			Return[nx]
+		];
+
+
+		(* this algorithm is much quicher on bigger expressions, maybe there should be a
+		switch for smaller ones to not use this?
+		Changed Nov 2003, RM
+		 *)
+
+		pali = Select[Cases2[nx, Pair], !FreeQ[#, LorentzIndex|Momentum]&];
+
+		If[ pali =!= {},
+			nx = nx /. Dispatch[Thread[pali -> oldExpandScalarProduct[pali]]]
+		];
+
+		nx
 	];
 
 oldExpandScalarProduct[x_,___Rule] :=
-(*
-If[(FCI /. {ru} /. Options[ExpandScalarProduct]),
-	FixedPoint[pairexpand1,FCI[x], 3]//MomentumExpandexpand,
-*)
 	FixedPoint[pairexpand1, x, 3]//MomentumExpand;
-(*
-	];
-*)
 
 ExpandScalarProduct[x_, y_ /;Head[y] =!= Rule] :=
-	scev[x, y];
+	scevdoit[x, y];
 
-(* Catch Pair[LorentzIndex[mu], Momentum[a] + Momentum [b] +...].
-	F.Orellana. 26/2-2003 *)
-(* simplify slightly, R. Merig, Sept. 13th 2003 *)
-extraMomRule = Pair[LorentzIndex[a__],
-							b : Plus[_. Momentum[__], _. Momentum[__]...]]  :>
-(*
-							b : Plus[(___*Momentum[__] | Momentum[__]),
-												(___*Momentum[__] | Momentum[__]) ...]]  :>
-*)
-							(Pair[LorentzIndex[a], #]& /@ b);
+(* Catch Pair[LorentzIndex[mu], Momentum[a] + Momentum [b] +...]. *)
+extraMomRule =
+	Pair[LorentzIndex[a__], b : Plus[_. Momentum[__], _. Momentum[__]...]]  :> (Pair[LorentzIndex[a], #]& /@ b);
 
 pairexpand1[x_] :=
 	x /. Pair->scevdoit /. extraMomRule;
 
-
-(* not always a good idea (IFPD)
-scev[x_,y_]:= MemSet[ scev[x,y], scevdoit[x,y] ];
-*)
-scev = scevdoit;
 scevdoit[x_,y_] :=
-	Distribute[ sceins@@
-											 ( Expand[ MomentumExpand/@{x,y} ] )
-										 ]/.sceins->sczwei/.
-										 sczwei(*->PairContract/.PairContract*)->Pair;
+	Distribute[sceins@@(Expand[ MomentumExpand/@{x,y} ])]/.
+	sceins->sczwei/.sczwei->Pair;
 
 sceins[0,_] :=
-	0;                               (*sceinsdef*)
+	0;
+
 sceins[a_LorentzIndex b_, c_] :=
 	b sceins[a, c];
+
 sceins[a_Momentum b_, c_] :=
 	b sceins[a, c];
+
 sczwei[ _[_],_[_,_Symbol-4] ] :=
-	0;             (*sczweidef*)
+	0;
+
 sczwei[ v_[x_,di_Symbol-4],w_[y_,di_Symbol] ] :=
 	sczwei[v[x,di-4],w[y,di-4]];
+
 sczwei[ w_[y_,_Symbol],v_[x_] ] :=
 	sczwei[ v[x],w[y] ];
 
