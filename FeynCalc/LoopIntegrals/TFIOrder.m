63a7cdf6dad3de7304ca089394f1b36725b8863b
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-08-28 16:14:36 +0200

    Added TFIOrder that can exploit some symmetries between Tarcer's scalar 2-loop integrals TFI.

---
+++ FeynCalc/LoopIntegrals/TFIOrder.m
@@ -0,0 +1,49 @@
+(* ::Package:: *)
+
+
+
+(* :Title: TFIOrder                                                       	*)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:	Orders the arguments of some TFI functions in a
+				standard way												*)
+
+(* ------------------------------------------------------------------------ *)
+
+TFIOrder::usage =
+"TFIOrder[expr] orders the arguments of some TFI functions in a \
+standard way.";
+
+
+(* ------------------------------------------------------------------------ *)
+
+Begin["`Package`"]
+End[]
+
+Begin["`TFIOrder`Private`"]
+
+Options[TFIOrder] = {};
+
+TFIOrder[expr_, OptionsPattern[]] :=
+	Block[{res,repRule},
+		repRule= {
+			(*Symmetry under the interchang q1 <-> q2*)
+			Tarcer`TFI[d_, p_, {{1, m1_}, {1, m2_}, {1, m3_}, {1, m4_}, {1, m5_}}] /; ! OrderedQ[{m1, m2}] :>
+				Tarcer`TFI[d, p, {{1, m2}, {1, m1}, {1, m4}, {1, m3}, {1, m5}}],
+			(*Symmetry under the shifts q1 -> q1-p and q2 -> q2-p, *)
+			Tarcer`TFI[d_, p_, {{1, m1_}, {1, m2_}, {1, m3_}, {1, m4_}, {1, m5_}}] /; ! OrderedQ[{{m1, m2}, {m3, m4}}] :>
+				Tarcer`TFI[d, p, {{1, m3}, {1, m4}, {1, m1}, {1, m2}, {1, m5}}]
+			};
+		res = expr//.repRule;
+		res
+	];
+
+
+FCPrint[1,"TFIOrder.m loaded."];
+End[]
