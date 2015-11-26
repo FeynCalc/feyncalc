6848ad6d18b121b22fee6fa857c306bde1fd00bd
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-11-26 22:37:34 +0100

    Fixed the problem with field arguments given as a sequence in FunctionalD + some refactoring.

--- FeynCalc/Feynman/FunctionalD.m
+++ FeynCalc/Feynman/FunctionalD.m
@@ -16,12 +16,12 @@
 
 FunctionalD::usage =
 "FunctionalD[expr,
-{QuantumField[name, LorentzIndex[mu], ..., SUNIndex[a]][p], ...}] calculates
-the functional derivative of expr with respect to the field list (with
-incoming momenta p, etc.) and does the fourier transform.
-
-FunctionalD[expr, {QuantumField[name, LorentzIndex[mu], ...
-SUNIndex[a]], ...}] calculates the functional derivative and does
+{QuantumField[name, LorentzIndex[mu], ..., SUNIndex[a]][p], ...}] calculates \
+the functional derivative of expr with respect to the QuantumField list (with \
+incoming momenta p, etc.) and does the fourier transform. \
+\n
+FunctionalD[expr, {QuantumField[name, LorentzIndex[mu], ... \
+SUNIndex[a]], ...}] calculates the functional derivative and does \
 partial integration but omits the x-space delta functions.";
 
 (* ------------------------------------------------------------------------ *)
@@ -31,39 +31,43 @@ End[]
 
 Begin["`FunctionalD`Private`"]
 
-(* ********************************************************************** *)
 (* products of metric tensors  and SU(N) deltas *)
-g[{}, {}] = d[{}, {}] = 1;
+g[{}, {}] =
+	1;
+d[{}, {}] =
+	1;
 g[{x__} ,{y__}] :=
 	g[{x}, {y}] =
-	(PairContract3[{x}[[1]], {y}[[1]]] g[Rest[{x}], Rest[{y}]]);
+		(PairContract3[{x}[[1]], {y}[[1]]] g[Rest[{x}], Rest[{y}]]);
+
 d[{x__} ,{y__}] :=
 	d[{x}, {y}] =
-	(SUNDeltaContract[{x}[[1]], {y}[[1]]] d[Rest[{x}], Rest[{y}]]);
-field = QuantumField;
-(* ********************************************************************** *)
+		(SUNDeltaContract[{x}[[1]], {y}[[1]]] d[Rest[{x}], Rest[{y}]]);
 
 ddl[FCPartialD[Momentum[OPEDelta]^m_]][p_] :=
 	(-1)^m I^m Pair[Momentum[p], Momentum[OPEDelta]]^m;
 
 ddl[][_] :=
 	1;
+
 ddl[pa:FCPartialD[Momentum[OPEDelta]..]][p_] :=
-	(-1)^Length[{pa}] I^Length[{pa}] *
-	Pair[Momentum[p], Momentum[OPEDelta]]^Length[{pa}];
+	(-1)^Length[{pa}] I^Length[{pa}] Pair[Momentum[p], Momentum[OPEDelta]]^Length[{pa}];
+
 ddl[pa:FCPartialD[Momentum[OPEDelta]]..][p_] :=
-	(-1)^Length[{pa}] I^Length[{pa}] *
-	Pair[Momentum[p], Momentum[OPEDelta]]^Length[{pa}];
+	(-1)^Length[{pa}] I^Length[{pa}] Pair[Momentum[p], Momentum[OPEDelta]]^Length[{pa}];
 
 ddl[FCPartialD[LorentzIndex[m_]]][p_] :=
 	(-I) dDelta[Momentum[p], LorentzIndex[m]];
+
 ddl[a___,FCPartialD[LorentzIndex[m_]], x___][p_] :=
 	(-I) dDelta[Momentum[p], LorentzIndex[m]] ddl[a, x][p];
 
 ddl[FCPartialD[Momentum[m_]]][p_] :=
 	(-I) dDelta[Momentum[p], Momentum[m]];
+
 ddl[a___,FCPartialD[Momentum[m_]], x___][p_] :=
 	(-I) dDelta[Momentum[p], Momentum[m]] ddl[a, x][p];
+
 ddl[a___,FCPartialD[Momentum[OPEDelta]^m_], x___][p_] :=
 	(-1)^m I^m Pair[Momentum[p], Momentum[OPEDelta]]^m  ddl[a, x][p];
 
@@ -74,67 +78,62 @@ dot2[a___,1,b___] :=
 
 
 (* functional differentation function:  FunctionalD *)
-FunctionalD[y_, fi_QuantumField, op___] :=
-	FunctionalD[y,{fi}, op];
+FunctionalD[y_, fi__QuantumField] :=
+	FunctionalD[y,{fi}];
+
+FunctionalD[y_, fi_QuantumField, op__] :=
+	FunctionalD[y,{fi,op}]/;Head[Last[{op}]]=!=List
+
+FunctionalD[y_, fi_QuantumField, op__] :=
+	FunctionalD[y,{fi,Sequence@@Most[{op}]},Last[{op}]]/;Head[Last[{op}]]===List
+
 (* product rule, recursive *)
-
-
 FunctionalD[y_, {fi__, a_ }, op___] :=
 	FunctionalD[FunctionalD[y, {a}, op], {fi}, op];
 
-(*Added ExplicitSUNIndex. F.Orellana, 16/9-2002*)
-FunctionalD[y_/;!FreeQ[y,field[__][___]],
-			{field[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex][pp___]}] :=
-	Block[ {xX},
+FunctionalD[y_/;!FreeQ[y,QuantumField[__][___]],
+		{QuantumField[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex][pp___]}] :=
+	Block[{xX},
 		D[If[ !FreeQ[y, FieldStrength],
 			Explicit[y],
 			y
 		] /.
-		field[a___, nam, b___][pe___] -> field[a,nam,b][pe][xX], xX] /.
-		{field[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
-		If[ {py} === {},
-			1,
-			ddl[py][pp]
-		] * g[{lor}, {li}] d[{sun}, {col}],
-		field[py___FCPartialD, nam, li___Momentum, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
-		If[ {py} === {},
-			1,
-			ddl[py][pp]
-		] * g[{lor}, {li}] d[{sun}, {col}]
-		} /.
-			{field[aa___, nam, bb___][pee___][xX] :> field[aa, nam, bb][pee],
-			SUNDelta  :> SUNDeltaContract, Pair :> PairContract3
-			} /. DOT -> dot2 /. dot2 -> DOT /. dDelta -> PairContract3/.
-				{SUNDeltaContract :> SUNDelta,  PairContract3 :> Pair}
+		QuantumField[a___, nam, b___][pe___] -> QuantumField[a,nam,b][pe][xX], xX] /. {
+		QuantumField[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
+			If[ {py} === {},
+				1,
+				ddl[py][pp]
+			] * g[{lor}, {li}] d[{sun}, {col}],
+		QuantumField[py___FCPartialD, nam, li___Momentum, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
+			If[ {py} === {},
+				1,
+				ddl[py][pp]
+			] * g[{lor}, {li}] d[{sun}, {col}]
+		} /. {	QuantumField[aa___, nam, bb___][pee___][xX] :> QuantumField[aa, nam, bb][pee],
+				SUNDelta  :> SUNDeltaContract,
+				Pair :> PairContract3 } /. DOT -> dot2 /. dot2 -> DOT /.
+				dDelta -> PairContract3 /. {SUNDeltaContract :> SUNDelta,  PairContract3 :> Pair}
 	];
 
-
-
-(*Definition below added by Frederik Orellana 25/2-1999*)
-
-FunctionalD[y_/;FreeQ[y,field[__][___]],
-{field[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex][pp___]}] :=
-	Block[ {xX},
-		D[y /.
-		field[a___, nam, b___] -> field[a,nam,b][xX], xX] /.
-		{field[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex]'[xX] :>
-		If[ {py} === {},
-			1,
-			ddl[py][pp]
-		] * g[{lor}, {li}] d[{sun}, {col}],
-		field[py___FCPartialD, nam, li___Momentum, col___SUNIndex|col___ExplicitSUNIndex]'[xX] :>
-		If[ {py} === {},
-			1,
-			ddl[py][pp]
-		] * g[{lor}, {li}] d[{sun}, {col}]
-		} /.
-			{field[aa___, nam, bb___][xX] -> field[aa, nam, bb] /.
+FunctionalD[y_/;FreeQ[y,QuantumField[__][___]],
+		{QuantumField[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex][pp___]}] :=
+	Block[{xX},
+			D[y /. QuantumField[a___, nam, b___] -> QuantumField[a,nam,b][xX], xX] /. {
+				QuantumField[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex]'[xX] :>
+					If[ {py} === {},
+						1,
+						ddl[py][pp]
+					] * g[{lor}, {li}] d[{sun}, {col}],
+				QuantumField[py___FCPartialD, nam, li___Momentum, col___SUNIndex|col___ExplicitSUNIndex]'[xX] :>
+					If[ {py} === {},
+						1,
+						ddl[py][pp]
+					] * g[{lor}, {li}] d[{sun}, {col}] } /. {
+					QuantumField[aa___, nam, bb___][xX] -> QuantumField[aa, nam, bb] /.
 					SUNDelta  :> SUNDeltaContract, Pair :> PairContract3
-			} /. DOT -> dot2 /. dot2 -> DOT /. dDelta -> PairContract3/.
-				{SUNDeltaContract :> SUNDelta,  PairContract3 :> Pair}
+					} /. DOT -> dot2 /. dot2 -> DOT /. dDelta -> PairContract3/.
+					{SUNDeltaContract :> SUNDelta,  PairContract3 :> Pair}
 	];
-
-(*End add*)
 
 (* stay in x-space, but omit deltafunctions *)
 		(* op: { first entry:
@@ -144,11 +143,10 @@ FunctionalD[y_/;FreeQ[y,field[__][___]],
 					final substitutions
 				}
 		*)
-FunctionalD[y_, {field[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex]
-				}, opin_:{-RightPartialD[#]&, {}}
-			] :=
+FunctionalD[y_, {QuantumField[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSUNIndex]},
+	opin_:{-RightPartialD[#]&, {}}] :=
 	Block[ {xX,  pard, r, lastrep = {}, op = opin, ddelta, partiald2},
-(* ddelta stands for the derivative of the delta funtion *)
+	(* ddelta stands for the derivative of the delta funtion *)
 		If[ Head[op] =!= List,
 			op = {op}
 		];
@@ -156,22 +154,19 @@ FunctionalD[y_, {field[nam_, lor___LorentzIndex, sun___SUNIndex|sun___ExplicitSU
 		If[ Length[op] > 1,
 			lastrep = Flatten[Rest[op]]
 		];
-		r =
-		(
-		D[If[ !FreeQ[y, FieldStrength],
-			Explicit[y],
-			y
-		] /.
-		field[a___, nam, b___] -> field[a,nam,b][][xX], xX]
-		) /.
-		{field[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
+		r = (D[	If[ !FreeQ[y, FieldStrength],
+					Explicit[y],
+					y
+				] /. QuantumField[a___, nam, b___] -> QuantumField[a,nam,b][][xX], xX]);
+		r = r /.
+		{QuantumField[py___FCPartialD, nam, li___LorentzIndex, col___SUNIndex|col___ExplicitSUNIndex][]'[xX] :>
 		If[ {py} === {},
 			1,
 			(ddelta[py] /. FCPartialD ->
 			partiald2)
 		] * g[{lor}, {li}] d[{sun}, {col}]
 		} /.
-			{field[aa___, nam, bb___][][xX] :> field[aa, nam, bb] ,
+			{QuantumField[aa___, nam, bb___][][xX] :> QuantumField[aa, nam, bb] ,
 			SUNDelta  :> SUNDeltaContract, PairContract3 :> PairContract
 			} /. Times -> DOT /. DOT -> dot2 /. dot2 -> DOT /.
 				{SUNDeltaContract :> SUNDelta, Pair :> PairContract} /.
