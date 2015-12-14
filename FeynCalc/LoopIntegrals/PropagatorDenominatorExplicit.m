0a6ef445e561e814621f58c238632a9444afc39e
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-12-14 15:12:05 +0100

    Exteneded PropagatorDenominatorExplicit to allow for changing the dimension of the propagators.

--- FeynCalc/LoopIntegrals/PropagatorDenominatorExplicit.m
+++ FeynCalc/LoopIntegrals/PropagatorDenominatorExplicit.m
@@ -1,29 +1,29 @@
-(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+(* ::Package:: *)
 
-(* :Title: PropagatorDenominatorExplicit *)
 
-(* :Author: Rolf Mertig *)
+
+(* :Title: PropagatorDenominatorExplicit									*)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:	Rewrite FADs into SPDs										*)
 
 (* ------------------------------------------------------------------------ *)
-(* :History: File created on 22 June '97 at 23:00 *)
-(* ------------------------------------------------------------------------ *)
-
-(* :Summary: PropagatorDenominatorExplicit *)
-
-(* ------------------------------------------------------------------------ *)
-
 
 PropagatorDenominatorExplicit::usage =
-"PropagatorDenominatorExplicit[exp] changes each occurence of
-PropagatorDenominator[a,b]
-in exp into 1/(ScalarProduct[a,a]-b^2) and replaces FeynAmpDenominator
-by Times.";
+"PropagatorDenominatorExplicit[exp] changes each occurence of \
+PropagatorDenominator[a,b] in exp into 1/(ScalarProduct[a,a]-b^2) and \
+replaces FeynAmpDenominator by Times.";
 
 PDEHead::usage =
-"PDEHead is an option of PropagatorDenominatorExplicit. It allows
-to wrap propagators into a specified head after applying
+"PDEHead is an option of PropagatorDenominatorExplicit. It allows \
+to wrap propagators into a specified head after applying \
 PropagatorDenominatorExpliciti. The default value is Identity.";
-
 
 (* ------------------------------------------------------------------------ *)
 
@@ -33,20 +33,35 @@ End[]
 Begin["`PropagatorDenominatorExplicit`Private`"]
 
 Options[PropagatorDenominatorExplicit] = {
+	FCI -> False,
+	Dimension -> False,
 	PDEHead->Identity
 }
 
-PropagatorDenominatorExplicit[x_, OptionsPattern[]] :=
-	If[ FreeQ[x, PropagatorDenominator],
-		x,
-		x //. {
-				PropagatorDenominator[a_  /; !FreeQ[a, Momentum] ,b_] :>
-				(1/Expand[ExpandScalarProduct[Pair[a, a]] - b^2]),
-				PropagatorDenominator[a_  /; FreeQ[a, Momentum] ,b_] :>
-				(1/Expand[ExpandScalarProduct[Pair[Momentum[a], Momentum[a]]] -
-						b^2]),
-				FeynAmpDenominator[c___] :> OptionValue[PDEHead][Times[c]]
-				}
+PropagatorDenominatorExplicit[expr_, OptionsPattern[]] :=
+	Block[{ex, dim, res, head},
+
+		If[	OptionValue[FCI],
+			ex = expr,
+			ex = FCI[expr]
+		];
+
+		dim = OptionValue[Dimension];
+		head = OptionValue[PDEHead];
+
+		res = ex //. {
+			PropagatorDenominator[a_  /; !FreeQ[a, Momentum] ,b_] :>
+			(1/Expand[ExpandScalarProduct[Pair[a, a]] - b^2]),
+
+			PropagatorDenominator[a_  /; FreeQ[a, Momentum] ,b_] :>
+			(1/Expand[ExpandScalarProduct[Pair[Momentum[a], Momentum[a]]] - b^2])
+		};
+
+		If[dim===False,
+			res = res /. FeynAmpDenominator[c___] :> head[Times[c]],
+			res = res /. FeynAmpDenominator[c___] :> head[ChangeDimension[Times[c],dim]]
+		];
+		res
 	];
 
 FCPrint[1,"PropagatorDenominatorExplicit.m loaded."];
