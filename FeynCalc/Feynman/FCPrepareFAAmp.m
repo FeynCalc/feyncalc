eb39e4d2bb10802864f2fd31f7226337b2bef43d
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2016-02-02 16:24:13 +0100

    Fixed FCPrepareFAAmp to convert all the known FeynArts objects in the expression (thanks to Sun Qingfeng)

--- FeynCalc/Feynman/FCPrepareFAAmp.m
+++ FeynCalc/Feynman/FCPrepareFAAmp.m
@@ -70,9 +70,9 @@ FCPrepareFAAmp[expr_, OptionsPattern[]] :=
 					FeynArts`MatrixTrace :> DiracTrace
 					}];
 		replist3 = {FeynArts`FAPropagatorDenominator[x__] :> FeynAmpDenominator[PropagatorDenominator[x]]};
-		temp = expr /. replist1 /. replist2 /. replist3;
+		temp = expr //. replist1 //. replist2 //. replist3;
 		If[ OptionValue[UndoChiralSplittings],
-			temp = temp/.{(a1__ DiracGamma[x_].DiracGamma[6] a2__ + a1__ DiracGamma[x_].DiracGamma[7] a2__) :> a1 DiracGamma[x] a2,
+			temp = temp//.{(a1__ DiracGamma[x_].DiracGamma[6] a2__ + a1__ DiracGamma[x_].DiracGamma[7] a2__) :> a1 DiracGamma[x] a2,
 			(a1__ DiracGamma[6] a2__ + a1__ DiracGamma[7] a2__) :> a1 a2}
 		];
 		temp
