a9e8745cbc1d3e8abe6006b2eb787956973f0078
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2016-02-13 17:30:55 +0100

    Added FCTraceExpand, a function that expands traces without evaluating them.

---
+++ FeynCalc/NonCommAlgebra/FCTraceExpand.m
@@ -0,0 +1,94 @@
+(* Wolfram Language package *)
+
+(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+
+(* :Title: FCTraceExpand														*)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Expands traces using linearity							    *)
+
+(* ------------------------------------------------------------------------ *)
+
+FCTraceExpand::usage =
+"FCTraceExpand[expr]  expands traces of Dirac and SU(N) matrices \
+using linearity of the trace. The traces themselves are not \
+evaluated.";
+
+(* ------------------------------------------------------------------------ *)
+
+Begin["`Package`"]
+End[]
+
+Begin["`FCTraceExpand`Private`"]
+
+dotSimp::usage="";
+
+Options[FCTraceExpand] = {
+	FCI -> False,
+	Momentum -> All,
+	DiracTrace -> True,
+	SUNTrace -> True,
+	DotSimplify -> True
+};
+
+FCTraceExpand[expr_, OptionsPattern[]] :=
+	Block[ {ex, moms,res, diracTraces, sunTraces},
+
+		moms = OptionValue[Momentum];
+		dotSimp = OptionValue[DotSimplify];
+
+		If[ OptionValue[FCI],
+			ex = expr,
+			ex = FCI[expr]
+		];
+
+		If[ FreeQ2[ex,{DiracTrace, SUNTrace}],
+			Return[ex]
+		];
+
+		If[	OptionValue[SUNTrace],
+			sunTraces = Cases2[ex, SUNTrace];
+			If[ sunTraces =!= {},
+				ex = ex /. Dispatch[Thread[sunTraces -> traceexpand[sunTraces]]]
+			];
+		];
+
+		If[	OptionValue[DiracTrace],
+			If [moms===All,
+				diracTraces = Cases2[ex, DiracTrace],
+				diracTraces = Select[Cases2[ex, DiracTrace], !FreeQ2[#, moms]&]
+			];
+
+			If[ diracTraces =!= {},
+				ex = ex /. Dispatch[Thread[diracTraces -> traceexpand[diracTraces]]]
+			]
+		];
+
+		res = ex;
+
+		res
+	];
+
+traceexpand[x_] :=
+	x /. {DiracTrace->expandDirac, SUNTrace->expandColor};
+
+expandDirac[x_] :=
+	If [dotSimp,
+		Distribute[DiracTrace@(Expand[DotSimplify[x]])],
+		Distribute[DiracTrace@(Expand[x])]
+	]
+
+expandColor[x_] :=
+	If [dotSimp,
+		Distribute[SUNTrace@(Expand[DotSimplify[x]])],
+		Distribute[SUNTrace@(Expand[x])]
+	]
+
+FCPrint[1,"FCTraceExpand.m loaded."];
+End[]
