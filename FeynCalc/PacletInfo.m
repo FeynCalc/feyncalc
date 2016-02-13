9d75033146673e0eec1535d472843f0526e3c4a8
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2016-02-13 17:49:56 +0100

    Version bump to 9.0.1

--- FeynCalc/PacletInfo.m
+++ FeynCalc/PacletInfo.m
@@ -1,17 +1,17 @@
 (* Paclet Info File *)
 
-(* created 2015/11/05*)
+(* created 2016/02/13*)
 
 Paclet[
-	Name -> "FeynCalc",
-	Version -> "9.0.0",
-	MathematicaVersion -> "8+",
-	Description -> "FeynCalc is a Mathematica package for symbolic evaluation of Feynman diagrams and algebraic calculations in quantum field theory and elementary particle physics.",
-	Creator -> "Rolf Mertig, Frederik Orellana, Vladyslav Shtabovenko",
-	Extensions ->
-		{
-			{"Documentation", Language -> "English", MainPage -> "Guides/FeynCalc"}
-		}
+    Name -> "FeynCalc",
+    Version -> "9.0.1",
+    MathematicaVersion -> "8+",
+    Description -> "FeynCalc is a Mathematica package for symbolic evaluation of Feynman diagrams and algebraic calculations in quantum field theory and elementary particle physics.",
+    Creator -> "Rolf Mertig, Frederik Orellana, Vladyslav Shtabovenko",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Guides/FeynCalc"}
+        }
 ]
 
 
