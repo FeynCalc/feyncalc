0a394469a9c4104f922a82ae4aaaae644158704a
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-01-25 16:56:07 +0100

    Fixed handling of BMHV in FCMultiLoopTID and blocked input in wrong dimensions.

--- FeynCalc/LoopIntegrals/FCMultiLoopTID.m
+++ FeynCalc/LoopIntegrals/FCMultiLoopTID.m
@@ -24,6 +24,14 @@ StandardForm];
 FCMultiLoopTID::failmsg =
 "Error! FCMultiLoopTID has encountered a fatal problem and must abort the \
 computation. The problem reads: `1`"
+
+
+FCMultiLoopTID::nomulti =
+"Warning! Your input contains 1-loop tensor integrals that depend on the given \
+loop momenta but no multi-loop tensor integrals. FCMultiLoopTID does not perform \
+1-loop tensor decompositions. For that you should use TID. The input expression \
+will not be processed further.";
+
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Package`"]
@@ -43,6 +51,7 @@ Options[FCMultiLoopTID] = {
 	DiracSimplify -> True,
 	ExpandScalarProduct -> True,
 	FCI -> False,
+	FCE -> False,
 	FCVerbose -> False,
 	FDS -> True,
 	ApartFF -> True
@@ -51,7 +60,7 @@ Options[FCMultiLoopTID] = {
 FCMultiLoopTID[expr_ , qs_List/; FreeQ[qs, OptionQ], OptionsPattern[]] :=
 	Block[	{	n, ex, rest,loopInts,intsUnique,
 				repRule,ruleUncontract,solsList,
-				null1, null2, res	},
+				null1, null2, res, rest1Loop, loopInts1Loop, intsUnique1Loop},
 
 		If [OptionValue[FCVerbose]===False,
 			mltidVerbose=$VeryVerbose,
@@ -60,7 +69,7 @@ FCMultiLoopTID[expr_ , qs_List/; FreeQ[qs, OptionQ], OptionsPattern[]] :=
 			];
 		];
 
-		If[	!FreeQ2[$ScalarProducts, {qs}],
+		If[	!FreeQ2[$ScalarProducts, qs],
 			Message[FCMultiLoopTID::failmsg, "Some loop momenta have scalar product rules attached to them. Evaluation aborted!"];
 			Abort[]
 		];
@@ -70,24 +79,28 @@ FCMultiLoopTID[expr_ , qs_List/; FreeQ[qs, OptionQ], OptionsPattern[]] :=
 			ex = FCI[expr]
 		];
 
-		FCPrint[1,"FCMultiLoopTID: Entering FCMultiLoopTID with: ", ex, FCDoControl->mltidVerbose];
+		FCPrint[3,"FCMultiLoopTID: Entering FCMultiLoopTID with: ", ex, FCDoControl->mltidVerbose];
 
 		n = OptionValue[Dimension];
 
 		ruleUncontract = {
-			DiracGamma[Momentum[x_, dim_: 4], dim_: 4] /;!FreeQ2[x, qs] :>
-			Uncontract[DiracGamma[Momentum[x, dim], dim], x],
-			Eps[a___,Momentum[x_, dim_: 4], b___] /;!FreeQ2[x, qs] :>
-			Uncontract[Eps[a,Momentum[x, dim], b], x]
+			DiracGamma[Momentum[x_, dim_:4], dim_:4] /;!FreeQ2[x, qs] :>
+				(tmpli=Unique[];  Pair[Momentum[x,dim],LorentzIndex[tmpli,dim]] DiracGamma[LorentzIndex[tmpli,dim],dim]),
+			Eps[a___,Momentum[x_, dim_:4], b___] /;!FreeQ2[x, qs] :>
+				(tmpli=Unique[];  Pair[Momentum[x,dim],LorentzIndex[tmpli,dim]] Eps[a, LorentzIndex[tmpli,dim], b])
 		};
-
 
 		If[ FreeQ2[ex,qs],
 			Return[ex]
 		];
 
-		If[	OptionValue[Contract],
+		If[	OptionValue[Contract] && !FreeQ[ex,LorentzIndex],
 			ex = Contract[ex]
+		];
+
+		If[	!FreeQ2[Union[FCGetDimensions[ex/.DiracGamma[5|6|7]:>null1]],{4,-4}] && !$BreitMaison,
+			Message[FCMultiLoopTID::failmsg,"Your input contains a mixture of 4- and D-dimensional quantities. This is in general not allowed in dimensional regularization, unless you are using the Breitenlohner-Maison-t'Hooft-Veltman scheme."];
+			Abort[]
 		];
 
 		If[	OptionValue[DiracSimplify] && !FreeQ2[ex,{DiracGamma,DiracSigma,Spinor}],
@@ -100,20 +113,43 @@ FCMultiLoopTID[expr_ , qs_List/; FreeQ[qs, OptionQ], OptionsPattern[]] :=
 			FCPrint[3,"FCMultiLoopTID: After first ApartFF: ", ex, FCDoControl->mltidVerbose]
 		];
 
+		(* Single out relevant loop momenta *)
 		ex = ex//DiracGammaExpand[#,Momentum->qs]&//ExpandScalarProduct[#,Momentum->qs,EpsEvaluate->True]&;
 
 		(*	The Dirac matrices and epsilon tensors could also be 4-dimensional. Then we need
 			to first uncontract and then convert the loop momenta to D dimensions	*)
-		ex = ex//.ruleUncontract /.
-			Pair[LorentzIndex[l_, d_: 4], Momentum[x_, d_: 4]] /;!FreeQ2[x, qs]&& d=!=n :>
-			ChangeDimension[Pair[LorentzIndex[l, d], Momentum[x, d]], n];
+		ex = ex//.ruleUncontract;
+
+
+		If[ $BreitMaison && !FreeQ[ex,LorentzIndex],
+
+			ex = ex /. {
+				Pair[Momentum[q_,n-4],LorentzIndex[i_,n-4]]/; MemberQ[qs,q]:>
+					(tmpli=Unique[];  Pair[Momentum[q,n],LorentzIndex[tmpli,n]] Pair[LorentzIndex[tmpli,n-4],LorentzIndex[i,n-4]]),
+				Pair[Momentum[q_],LorentzIndex[i_]]/; MemberQ[qs,q] :>
+					(tmpli=Unique[];  Pair[Momentum[q,n],LorentzIndex[tmpli,n]] Pair[LorentzIndex[tmpli],LorentzIndex[i]])
+			};
+			If[ !FreeQ2[ex, {Pair[Momentum[q_/;MemberQ[qs,q], n-4],LorentzIndex[_,n-4]],Pair[Momentum[q_/;MemberQ[qs,q]],LorentzIndex[_]]}],
+				Message[FCMultiLoopTID::failmsg,"Failed to eliminate 4 and D-4 dimensional loop momenta."];
+				Abort[]
+			];
+			FCPrint[2,"FCMultiLoopTID: Tensor parts after handling 4 and D-4 dimensional loop momenta: ", ex, FCDoControl->tidVerbose];
+		];
 
 		FCPrint[3,"FCMultiLoopTID: After Uncontract: ", ex, FCDoControl->mltidVerbose];
 
 		{rest,loopInts,intsUnique} = FCLoopExtract[ex, qs,loopHead,
 			FCLoopSplit -> {4},MultiLoop->True,FCI->True,PaVe->False];
 
+		{rest1Loop,loopInts1Loop,intsUnique1Loop} = FCLoopExtract[ex, qs,loopHead,
+			FCLoopSplit -> {4},MultiLoop->False,FCI->True,PaVe->False];
+
 		FCPrint[2,"FCMultiLoopTID: List of the unique integrals: ", intsUnique, FCDoControl->mltidVerbose];
+
+		If[ intsUnique==={} && intsUnique1Loop=!={},
+			Message[FCMultiLoopTID::nomulti];
+			Return[ex]
+		];
 
 
 		(*	Apply fdsOneLoop to each of the unique loop integrals	*)
@@ -151,6 +187,10 @@ FCMultiLoopTID[expr_ , qs_List/; FreeQ[qs, OptionQ], OptionsPattern[]] :=
 		If[	OptionValue[Collecting],
 			res = Collect2[res,FeynAmpDenominator,Sequence@@qs];
 			FCPrint[3,"FCMultiLoopTID: After Collect2: ", res, FCDoControl->mltidVerbose]
+		];
+
+		If[	OptionValue[FCE],
+			res = FCE[res]
 		];
 
 		FCPrint[3, "FCMultiLoopTID: Leaving with: ", res, FCDoControl->mltidVerbose];
