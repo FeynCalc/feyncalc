d217744e2c876097224d27e0e6ce1d780d6d6017
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-03-09 19:27:09 +0100

    Added an example for calculations with the Euler-Heisenberg Lagrangian ( the model is generated using FeynArts).

---
+++ FeynCalc/Examples/FeynRules/EulerHeisenberg/GenerateModelEulerHeisenberg.m
@@ -0,0 +1,47 @@
+(* ::Package:: *)
+
+(* :Title: GenerateModelEulerHeisenberg									*)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Generates FeynArts model for the Euler-Heisenberg Lagrangian *)
+
+(* ------------------------------------------------------------------------ *)
+
+
+
+(* ::Section:: *)
+(*Euler-Heisenberg Lagrangian model for FeynArts*)
+
+
+(* ::Subsection:: *)
+(*Load FeynRules*)
+
+
+FR$Parallel=False;
+$FeynRulesPath=SetDirectory[$UserBaseDirectory<>"/Applications/FeynRules"];
+<<FeynRules`;
+
+
+(* ::Subsection:: *)
+(*Load FeynRules model*)
+
+
+frModelPath=FileNameJoin[{$UserBaseDirectory,"Applications","FeynCalc","Examples","FeynRules","EulerHeisenberg","EulerHeisenberg.fr"}];
+LoadModel[frModelPath];
+
+
+(* ::Subsection:: *)
+(*Create FeynArts model*)
+
+
+SetDirectory[FileNameJoin[{$UserBaseDirectory,"Applications","FeynCalc","FeynArts","Models"}]];
+WriteFeynArtsOutput[LEH,Output->"EulerHeisenberg",CouplingRename->False];
+
+
+
---
+++ FeynCalc/Examples/FeynRules/EulerHeisenberg/ValidateModelEulerHeisenberg.m
@@ -0,0 +1,85 @@
+(* ::Package:: *)
+
+(* :Title: ValidateModelEulerHeisenberg									*)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Validates FeynArts model for the Euler-Heisenberg Lagrangian *)
+
+(* ------------------------------------------------------------------------ *)
+
+
+
+(* ::Section:: *)
+(*Load FeynCalc and FeynArts*)
+
+
+If[ $FrontEnd === Null,
+		$FeynCalcStartupMessages = False;
+		Print["Validation of the FeynArts model for the Euler-Heisenberg Lagrangian"];
+];
+If[$Notebooks === False, $FeynCalcStartupMessages = False];
+$LoadFeynArts= True;
+<<FeynCalc`
+$FAVerbose = 0;
+
+
+(* ::Section:: *)
+(*Patch the generated models to work with FeynCalc*)
+
+
+FAPatch[PatchModelsOnly->True];
+
+
+(* ::Section:: *)
+(*Check vertices*)
+
+
+diag=InsertFields[CreateTopologies[0,2 -> 2],{V[1],V[1]} -> {V[1],V[1]},
+InsertionLevel -> {Classes},Model -> FileNameJoin[{"EulerHeisenberg","EulerHeisenberg"}],
+GenericModel->FileNameJoin[{"EulerHeisenberg","EulerHeisenberg"}]];
+
+
+amp=FCFAConvert[FCPrepareFAAmp[CreateFeynAmp[diag, Truncated -> True,PreFactor->1]],
+IncomingMomenta->{p1,p2},OutgoingMomenta->{p2,p3},UndoChiralSplittings->True,
+DropSumOver->True, List->False,SMP->True];
+
+
+c1Part=1/SMP["m_e"]^4 32 I c1 (FV[p1,Lor2] FV[p2,Lor1] FV[p2,Lor4] FV[p3,Lor3]-
+FV[p2,Lor4] FV[p3,Lor3] MT[Lor1,Lor2] SP[p1,p2]-FV[p2,Lor4] FV[p3,Lor2] MT[Lor1,Lor3] SP[p1,p2]-
+FV[p2,Lor2] FV[p2,Lor3] MT[Lor1,Lor4] SP[p1,p3]+MT[Lor1,Lor4] MT[Lor2,Lor3] SP[p1,p3] SP[p2,p2]+
+FV[p1,Lor4] FV[p3,Lor1] (FV[p2,Lor2] FV[p2,Lor3]-MT[Lor2,Lor3] SP[p2,p2])-
+FV[p1,Lor2] FV[p2,Lor1] MT[Lor3,Lor4] SP[p2,p3]+MT[Lor1,Lor3] MT[Lor2,Lor4] SP[p1,p2] SP[p2,p3]+
+MT[Lor1,Lor2] MT[Lor3,Lor4] SP[p1,p2] SP[p2,p3]+FV[p1,Lor3] FV[p2,Lor1] (FV[p2,Lor4] FV[p3,Lor2]-MT[Lor2,Lor4] SP[p2,p3]));
+
+
+c2Part=1/SMP["m_e"]^4 8 I c2 (-FV[p2,Lor4] FV[p3,Lor3] MT[Lor1,Lor2] SP[p1,p2]-
+FV[p2,Lor4] FV[p3,Lor2] MT[Lor1,Lor3] SP[p1,p2]-FV[p2,Lor3] FV[p3,Lor2] MT[Lor1,Lor4] SP[p1,p2]-
+FV[p2,Lor2] FV[p3,Lor3] MT[Lor1,Lor4] SP[p1,p2]-FV[p2,Lor3] FV[p2,Lor4] MT[Lor1,Lor2] SP[p1,p3]-
+FV[p2,Lor2] FV[p2,Lor4] MT[Lor1,Lor3] SP[p1,p3]-FV[p1,Lor4] FV[p3,Lor3] MT[Lor1,Lor2] SP[p2,p2]-
+FV[p1,Lor4] FV[p3,Lor2] MT[Lor1,Lor3] SP[p2,p2]+FV[p1,Lor3] FV[p3,Lor2] MT[Lor1,Lor4] SP[p2,p2]+
+FV[p1,Lor2] FV[p3,Lor3] MT[Lor1,Lor4] SP[p2,p2]+MT[Lor1,Lor3] MT[Lor2,Lor4] SP[p1,p3] SP[p2,p2]+
+MT[Lor1,Lor2] MT[Lor3,Lor4] SP[p1,p3] SP[p2,p2]+FV[p3,Lor1] (-2 FV[p2,Lor4] MT[Lor2,Lor3] SP[p1,p2]
++FV[p2,Lor3] MT[Lor2,Lor4] SP[p1,p2]+FV[p2,Lor2] (FV[p1,Lor3] FV[p2,Lor4]+MT[Lor3,Lor4] SP[p1,p2])-
+FV[p1,Lor3] MT[Lor2,Lor4] SP[p2,p2]+FV[p1,Lor2] (FV[p2,Lor3] FV[p2,Lor4]-MT[Lor3,Lor4] SP[p2,p2]))+
+FV[p1,Lor4] FV[p2,Lor3] MT[Lor1,Lor2] SP[p2,p3]+FV[p1,Lor4] FV[p2,Lor2] MT[Lor1,Lor3] SP[p2,p3]-
+FV[p1,Lor3] FV[p2,Lor2] MT[Lor1,Lor4] SP[p2,p3]-FV[p1,Lor2] FV[p2,Lor3] MT[Lor1,Lor4] SP[p2,p3]+
+2 MT[Lor1,Lor4] MT[Lor2,Lor3] SP[p1,p2] SP[p2,p3]+MT[Lor1,Lor3] MT[Lor2,Lor4] SP[p1,p2] SP[p2,p3]+
+MT[Lor1,Lor2] MT[Lor3,Lor4] SP[p1,p2] SP[p2,p3]+FV[p2,Lor1] ((FV[p1,Lor4] FV[p2,Lor3]+
+FV[p1,Lor3] FV[p2,Lor4]) FV[p3,Lor2]+FV[p1,Lor2] FV[p2,Lor4] FV[p3,Lor3]+
+2 FV[p2,Lor4] MT[Lor2,Lor3] SP[p1,p3]-FV[p2,Lor3] MT[Lor2,Lor4] SP[p1,p3]+
+FV[p2,Lor2] (FV[p1,Lor4] FV[p3,Lor3]-MT[Lor3,Lor4] SP[p1,p3])-2 FV[p1,Lor4] MT[Lor2,Lor3] SP[p2,p3]-
+FV[p1,Lor3] MT[Lor2,Lor4] SP[p2,p3]-FV[p1,Lor2] MT[Lor3,Lor4] SP[p2,p3]));
+
+
+(* ::Subsection:: *)
+(*Check*)
+
+
+Print["Check vertices in the Euler-Heisenberg Lagrangian model: ",
+			If[{Simplify[FCE[SelectNotFree2[amp,c1]]-c1Part],Simplify[FCE[SelectNotFree2[amp,c2]]-c2Part]}===Table[0,2], "CORRECT.", "!!! WRONG !!!"]];
