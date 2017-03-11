f7797f05a32d92856b2a6a48384e168bb0858d3d
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-03-11 23:14:43 +0100

    Improved FCRenameDummyIndices to work with Cartesian objects.

--- FeynCalc/Lorentz/FCRenameDummyIndices.m
+++ FeynCalc/Lorentz/FCRenameDummyIndices.m
@@ -9,7 +9,7 @@
 	Copyright (C) 2014-2016 Vladyslav Shtabovenko
 *)
 
-(* :Summary:  Rename dummy Lorentz and SU(N) indices *)
+(* :Summary:  Rename dummy Lorentz, Cartesian and SU(N) indices 			*)
 
 (* ------------------------------------------------------------------------ *)
 
@@ -21,6 +21,10 @@ square of it. For example, the square of GA[a, l, a] equals 16. However, \
 if you forget to rename the dummy indices and compute GA[a, l, a, a, l, a] \
 instead of GA[a, l, a, b, l, b], you will get 64.";
 
+FCRenameDummyIndices::failmsg =
+"Error! FCRenameDummyIndices has encountered a fatal problem and must abort the computation. \
+The problem reads: `1`"
+
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Package`"]
@@ -28,37 +32,69 @@ End[]
 
 Begin["`FCRenameDummyIndices`Private`"]
 
-FCRenameDummyIndices[expr_List] :=
-	Map[FCRenameDummyIndices[#] &, expr];
+Options[FCRenameDummyIndices] = {
+	FCI -> False,
+	FCE -> False,
+	Head -> {LorentzIndex, CIndex, SUNIndex, SUNFIndex}
+};
 
-FCRenameDummyIndices[expr_] :=
-	Block[ {indexList = {}, replacementList, exprFCI},
+FCRenameDummyIndices[expr_List, opts:OptionsPattern[]] :=
+	Map[FCRenameDummyIndices[#,opts] &, expr];
 
-		If[	!FreeQ2[{expr}, FeynCalc`Package`NRStuff],
-			Message[FeynCalc::nrfail];
+FCRenameDummyIndices[expr_, OptionsPattern[]] :=
+	Block[ {indexList = {}, replacementList, exprFCI, ex, res, heads, patt},
+
+		heads = OptionValue[Head];
+
+		If[ !OptionValue[FCI],
+			exprFCI = FCI[expr],
+			exprFCI = expr
+		];
+
+		If[	DummyIndexFreeQ[exprFCI, heads],
+			Return[exprFCI]
+		];
+
+		ex = exprFCI;
+		exprFCI = exprFCI // DotSimplify[#,FCI->False]& // Expand;
+
+		If[ Head[heads]=!=List || heads==={},
+			Message[FCRenameDummyIndices::failmsg, "You did not specify which index heads should be renamed."];
 			Abort[]
 		];
 
-		exprFCI = expr // FCI // DotSimplify // Expand;
+		If[	Length[heads]>1,
+			patt = Alternatives@@heads,
+			patt = Identity@@heads
+		];
+
+
 		If[ Head[exprFCI]===Plus,
 			indexList =
-				Map[Tally, Map[Cases[#, (LorentzIndex | SUNIndex | SUNFIndex)[ind_, ___]/;(Head[ind]=!=Upper &&
-						Head[ind]=!=Lower) :> ind,
-						Infinity]&,Apply[List, exprFCI]]]// Flatten[#, 1] & // Union;,
+				Map[Tally, Map[Cases[#, (patt)[ind_, ___]/;(Head[ind]=!=Upper &&
+						Head[ind]=!=Lower) :> ind, Infinity]&,Apply[List, exprFCI]]]// Flatten[#, 1] & // Union,
 			indexList =
-				Cases[exprFCI, (LorentzIndex | SUNIndex | SUNFIndex)[ind_,___]/;(Head[ind]=!=Upper &&
-						Head[ind]=!=Lower) :> ind,
-						Infinity] // Tally;
+				Cases[exprFCI, (patt)[ind_,___]/;(Head[ind]=!=Upper && Head[ind]=!=Lower) :> ind, Infinity] // Tally;
 		];
+
 		FCPrint[1,"List of indices to be randomized: ", StandardForm[indexList]];
+
 		If[ Select[indexList, ((#[[2]]) > 2) &]=!={},
-			FCPrint[0,"Warning! The expression  ", exprFCI, " violates Einstein notation!"];
-			FCPrint[0,"Following indices appear more than twice: ", Select[indexList, ((#[[2]]) > 2) &]];
+			Message[FCRenameDummyIndices::failmsg, "The input expression violates Einstein summation. Some dummy indices appear more than twice."];
+			Abort[]
 		];
-		replacementList = Map[Rule[#[[1]], $AL[Unique[]]] &, Cases[indexList, {_, 2}]];
+
+		replacementList = Map[Rule[(h: patt)[#[[1]],di___], h[$AL[Unique[]],di]] &, Cases[indexList, {_, 2}]];
+
 		FCPrint[2,"List of replacement rules: ", replacementList];
-		result = FCI[expr] //. replacementList;
-		result
+
+		res = ex //. replacementList;
+
+		If[	OptionValue[FCE],
+			res = FCE[res]
+		];
+
+		res
 	]
 
 FCPrint[1,"FCRenameDummyIndices.m loaded."];
