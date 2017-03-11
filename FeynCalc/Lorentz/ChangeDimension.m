4827b5d3dde8e3ed2316e60f081c3ff45b01076e
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-03-11 22:10:08 +0100

    Improved and refactored ChangeDimension including support for new Cartesian objects.

--- FeynCalc/Lorentz/ChangeDimension.m
+++ FeynCalc/Lorentz/ChangeDimension.m
@@ -1,22 +1,29 @@
+(* ::Package:: *)
+
 (* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
 
-(* :Title: ChangeDimension *)
+(* :Title: ChangeDimension													*)
 
-(* :Author: Rolf Mertig *)
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
 
-(* ------------------------------------------------------------------------ *)
-(* :History: File created on 22 June '97 at 22:58 *)
-(* ------------------------------------------------------------------------ *)
-
-(* :Summary: change dimension from 4 to D or back *)
+(* :Summary:  Change dimension of Lorentz and Cartesian tensors				*)
 
 (* ------------------------------------------------------------------------ *)
 
 ChangeDimension::usage=
-"ChangeDimension[exp, dim] changes all LorentzIndex and Momenta in \
+"ChangeDimension[exp, dim] changes all LorentzIndex and Momentum objects in \
 exp to dimension dim (and thus also Dirac slashes and Dirac matrices \
-in FeynCalcInternal-representation).";
+in FeynCalcInternal-representation). CIndex and CMomentum objects become \
+changed to dimension dim-1.";
 
+ChangeDimension::failmsg =
+"Error! ChangeDimension has encountered a fatal problem and must abort \
+the computation. The problem reads: `1`"
 
 (* ------------------------------------------------------------------------ *)
 
@@ -25,36 +32,61 @@ End[]
 
 Begin["`ChangeDimension`Private`"]
 
-ChangeDimension[x_, diim_] :=
-	Block[
-		{xx = FeynCalcInternal[x], dirGG, dirGAMM, pAiR, ld , md,spi4},
+Options[ChangeDimension] ={
+	FCI -> False,
+	FCE -> False
+};
 
-		If[	!FreeQ2[{xx}, FeynCalc`Package`NRStuff],
-			Message[FeynCalc::nrfail];
+ChangeDimension[ex_, dim_, OptionsPattern[]] :=
+	Block[
+		{	expr, res, tmp, lorentzDim, cartesianDim,
+			holdPair, holdDiracGamma, holdCPair, holdEps, holdPauliSigma},
+
+		If[ OptionValue[FCI],
+			expr = ex,
+			expr = FCI[ex]
+		];
+
+		Switch[ dim,
+			4,
+				lorentzDim = 4;
+				cartesianDim = 3,
+			_Symbol,
+				lorentzDim = dim;
+				cartesianDim = dim-1,
+			_Symbol -4,
+				lorentzDim = dim;
+				cartesianDim = dim,
+			_,
+			Message[ChangeDimension::failmsg, "Unsupported choice of dimension!"];
 			Abort[]
 		];
 
-		If[ diim === 4,
-			xx = xx /. {LorentzIndex[a_,___] :> LorentzIndex[a] ,
-									Momentum[b_,___]     :> Momentum[b],
-									DiracGamma[c_,___]   :> DiracGamma[c]},
-			ld[a_,___] :=
-				LorentzIndex[a, diim];
-			md[a_,___] :=
-				Momentum[a, diim];
-			(*spi4[a_, b__] :=
-				Spinor[a /. Momentum[bbb_, ___] -> Momentum[bbb], b]*);
-			dirGG[aa_,___] :=
-				dirGAMM[aa, diim]/; !MatchQ[aa,5|6|7];
-			dirGG[(aa: 5|6|7)] :=
-				dirGAMM[aa];
-			xx = xx /. Pair -> pAiR /. DiracGamma -> dirGAMM;
-			xx = xx /. {Momentum :> md, LorentzIndex -> ld} /.
-								{md :> Momentum, ld :> LorentzIndex} ;
-			xx = xx /. dirGAMM -> dirGG/.  dirGAMM -> DiracGamma /. pAiR -> Pair;
-			(*xx = xx /. Spinor -> spi4*);
+		tmp = expr /. DiracGamma -> holdDiracGamma /. PauliSigma -> holdPauliSigma /. Pair-> holdPair /. CPair->holdCPair  /. Eps -> holdEps;
+
+		tmp = tmp /. holdDiracGamma[(z: 5|6|7)] :> DiracGamma[z];
+
+		tmp = tmp /. {
+			LorentzIndex[z_,___] :> LorentzIndex[z,lorentzDim],
+			CIndex[z_,___] :> CIndex[z,cartesianDim],
+			Momentum[z_,___] :> Momentum[z,lorentzDim],
+			CMomentum[z_,___] :> CMomentum[z,cartesianDim]
+		};
+
+		tmp = tmp /. {
+			holdDiracGamma[z_,___] :> holdDiracGamma[z,lorentzDim],
+			holdPauliSigma[z_,___] :> holdPauliSigma[z,cartesianDim]
+		};
+
+		res = tmp /. holdDiracGamma -> DiracGamma /. holdPauliSigma -> PauliSigma /. holdPair -> Pair /. holdCPair -> CPair /. holdEps -> Eps;
+
+
+		If[	OptionValue[FCE],
+			res = FCE[res]
 		];
-		xx
+
+		res
+
 	];
 
 FCPrint[1,"ChangeDimension.m loaded"];
