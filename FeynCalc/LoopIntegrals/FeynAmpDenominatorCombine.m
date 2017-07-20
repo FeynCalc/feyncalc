0d764f977721ba08518bf868a7f44511d0a831e8
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-07-21 00:55:34 +0200

    Improved FeynAmpDenominatorCombine to support the FCI option.

--- FeynCalc/LoopIntegrals/FeynAmpDenominatorCombine.m
+++ FeynCalc/LoopIntegrals/FeynAmpDenominatorCombine.m
@@ -26,13 +26,14 @@ Begin["`FeynAmpDenominatorCombine`Private`"]
 
 feyncomb[] =
 	1;
+
 feyncomb /:
 	feyncomb[a__] feyncomb[b__] :=
 		feyncomb[a, b];
 
 feyncomb /:
 	feyncomb[a__]^n_Integer?Positive :=
-		feyncomb @@ Flatten[Table[{a}, {ii, n}]];
+		feyncomb @@ Flatten[Table[{a}, {n}]];
 
 lev[PropagatorDenominator[a_, 0], PropagatorDenominator[b_, 0]] :=
 	If[ Length[Variables[a]] < Length[Variables[b]],
@@ -43,17 +44,34 @@ lev[PropagatorDenominator[a_, 0], PropagatorDenominator[b_, 0]] :=
 fdsor[a__] :=
 	Apply[FeynAmpDenominator, Sort[MomentumExpand[{a}], lev]];
 
-mfci[y_] :=
-	If[ FreeQ[y, FeynAmpDenominator],
-		FeynCalcInternal[y],
-		y
+Options[FeynAmpDenominatorCombine] = {
+	FCI -> False
+};
+
+FeynAmpDenominatorCombine[expr_, OptionsPattern[]] :=
+	Block[{ex,res},
+
+		If[ OptionValue[FCI],
+			ex = expr,
+			ex = FCI[expr]
+		];
+
+		If[FreeQ[ex,FeynAmpDenominator],
+			Return[ex]
+		];
+
+		res = Expand2[ex, FeynAmpDenominator] /.
+			FeynAmpDenominator -> feyncomb /. feyncomb -> fdsor;
+
+		res
+
+
 	];
 
-FeynAmpDenominatorCombine[x_] := (
 
-	Expand2[x//mfci, FeynAmpDenominator]  /.
-	FeynAmpDenominator -> feyncomb /. feyncomb -> fdsor
-	);
+
+
+
 
 FCPrint[1,"FeynAmpDenominatorCombine.m loaded."];
 End[]
