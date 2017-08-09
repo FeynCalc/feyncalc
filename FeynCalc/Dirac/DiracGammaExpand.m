53fbf5427b8e7e807bea7d20119935b981847b36
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2017-08-10 00:16:51 +0800

    Improved and refactored DiracGammaExpand to use DiracSigmaExpand for applying linearity to DiracSigma.

--- FeynCalc/Dirac/DiracGammaExpand.m
+++ FeynCalc/Dirac/DiracGammaExpand.m
@@ -20,7 +20,7 @@ DiracGammaExpand::usage =
 exp into (DiracGamma[Momentum[a]] + DiracGamma[Momentum[b]] + ...).";
 
 DiracGammaExpand::fail =
-"Something went while expanding momenta contracted with Dirac matrices.
+"Something went wrong while expanding momenta contracted with Dirac matrices.
 Evaluation aborted! "
 
 (* ------------------------------------------------------------------------ *)
@@ -31,33 +31,29 @@ End[]
 Begin["`DiracGammaExpand`Private`"]
 
 Options[DiracGammaExpand] = {
+	DiracSigmaExpand -> True,
+	FCE -> False,
 	FCI -> False,
 	Momentum -> All
 };
 
 DiracGammaExpand[expr_, OptionsPattern[]] :=
-	Block[{x,null1,null2, uniqList, solsList, repRule, momList,gaev,gaevlin,rud},
-
-	gaev[xx_,di_:4] :=
-		gaevlin[Expand[xx//MomentumExpand, Momentum], di];
-
-	gaevlin[xx_Plus, di_:4] :=
-		Map[gaevlin[#, di]&, xx];
+	Block[{ex, null1, null2, uniqList, solsList, repRule, momList, rud, res},
 
 	momList = OptionValue[Momentum];
 
 	If[	!OptionValue[FCI],
-		x = FCI[expr],
-		x = expr
+		ex = FCI[expr],
+		ex = expr
 	];
 
 	(*	Nothing to do...	*)
-	If[	FreeQ[x,DiracGamma],
-		Return[x]
+	If[	FreeQ[ex,DiracGamma],
+		Return[ex]
 	];
 
 	(* List of all the unique Feynman slashes	*)
-	uniqList = Cases[x+null1+null2, DiracGamma[arg_ /; ! FreeQ2[{arg}, {Momentum,CartesianMomentum}],___], Infinity]//DeleteDuplicates//Sort;
+	uniqList = Cases[ex+null1+null2, DiracGamma[arg_ /; ! FreeQ2[{arg}, {Momentum,CartesianMomentum}],___], Infinity]//DeleteDuplicates//Sort;
 
 	(*	If the user specified to perform expansion only for some
 		special slashed momenta, let's do it	*)
@@ -74,9 +70,25 @@ DiracGammaExpand[expr_, OptionsPattern[]] :=
 		Abort[]
 	];
 
-	x /. Dispatch[repRule]
+	res = ex /. Dispatch[repRule];
+
+	If[	OptionValue[DiracSigmaExpand],
+		res = DiracSigmaExpand[res, FCI->True, DiracGammaExpand->False, Momentum -> momList]
+	];
+
+	If[	OptionValue[FCE],
+		res = FCE[res]
+	];
+
+	res
 
 	];
+
+gaev[xx_,di_:4] :=
+	gaevlin[Expand[MomentumExpand[xx], Momentum], di];
+
+gaevlin[xx_Plus, di_:4] :=
+	Map[gaevlin[#, di]&, xx];
 
 FCPrint[1,"DiracGammaExpand.m loaded."];
 End[]
