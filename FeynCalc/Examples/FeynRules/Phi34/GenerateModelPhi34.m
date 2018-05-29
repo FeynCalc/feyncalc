a19450d4c6ad95ddc635dac5124f8a4fd74b5fe4
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2018-05-29 12:01:52 +0800

    Added a FeynRules model with phi^3 and phi^4 interactions.

---
+++ FeynCalc/Examples/FeynRules/Phi34/GenerateModelPhi34.m
@@ -0,0 +1,55 @@
+(* ::Package:: *)
+
+(* :Title: GenerateModelPhi34												*)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2018 Rolf Mertig
+	Copyright (C) 1997-2018 Frederik Orellana
+	Copyright (C) 2014-2018 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Generates FeynArts model for the scalar theory 		
+	with phi^3 and phi^4 interactions								*)
+
+(* ------------------------------------------------------------------------ *)
+
+
+
+(* ::Section:: *)
+(*phi^3+phi^4 model for FeynArts*)
+
+
+(* ::Subsection:: *)
+(*Load FeynRules*)
+
+
+FR$Parallel=False;
+$FeynRulesPath=SetDirectory[$UserBaseDirectory<>"/Applications/FeynRules"];
+<<FeynRules`;
+
+
+(* ::Subsection:: *)
+(*Load FeynRules model*)
+
+
+frModelPath=FileNameJoin[{$UserBaseDirectory,"Applications","FeynCalc","Examples","FeynRules","Phi34","Phi34.fr"}];
+LoadModel[frModelPath];
+
+
+(* ::Subsection:: *)
+(*Create FeynArts model*)
+
+
+FR$Loop=True;
+SetDirectory[FileNameJoin[{$UserBaseDirectory,"Applications","FeynCalc","FeynArts","Models"}]];
+WriteFeynArtsOutput[LPhi34,Output->"Phi34",CouplingRename->False];
+
+
+
+
+
+
+
+
+
