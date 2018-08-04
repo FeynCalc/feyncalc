8778efb7bcf5bbf89d2c0ea5fa184df288c124a5
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2018-08-04 14:41:38 +0800

    Added FCLoopBasisCreateScalarProducts, an auxiliary function that creates a list of all loop-momentum dependent scalar products from the given lists of loop and external momenta.

--- FeynCalc/LoopIntegrals/FCLoopBasis.m
+++ FeynCalc/LoopIntegrals/FCLoopBasis.m
@@ -40,6 +40,13 @@ FCLoopBasisGetSize::usage =
 "FCLoopBasisGetSize[n1,n2] returns the number of linearly independent propagators \
 for a topology that contains n1 loop momenta and n2 external momenta."
 
+FCLoopBasisCreateScalarProducts::usage=
+"FCLoopBasisCreateScalarProducts[{q1, q2, ...},{p1, p2,...},{d1, d2, ...}, head] generates \
+a list of all loop-momentum dependent scalar products made out of the loop momenta q1, q2, ... and \
+external momenta p1, p2, ... in the space-time dimensions d1, d2, .... The argument head can \
+be Pair to generate Lorentzian scalar products or CartesianPair to generate Cartesian scalar \
+products."
+
 FCLoopBasis::unknownmoms =
 "Error! Loop integral `1` depends on momenta that were not specified or it doesn't depend on \
 some of the specified momenta. Evaluation aborted.";
@@ -73,6 +80,10 @@ scalar products that do not depend on the loop momenta are discarded, unless the
 FCLoopBasisIntegralToTopology::failmsg =
 "Error! FCLoopBasisIntegralToTopology has encountered a fatal problem and must abort the computation. \
 The problem reads: `1`"
+
+FCLoopBasisCreateScalarProducts::failmsg =
+"Error! FCLoopBasisCreateScalarProducts encountered a fatal problem and must abort the computation. \
+The problem reads: `1`";
 
 FCLoopBasisExtract::nodims=
 "Error! FCLoopBasisExtract is unable to build up a list of possible scalar products, because the supplied \
@@ -142,6 +153,10 @@ Options[FCLoopBasisSplit] = {
 	FeynAmpDenominatorCombine -> True,
 	Head -> FCGV["LoopInt"],
 	List -> True
+};
+
+Options[FCLoopBasisCreateScalarProducts] = {
+	FCE -> False
 };
 
 Options[FCLoopBasisIntegralToTopology] = {
@@ -272,6 +287,7 @@ FCLoopBasisIntegralToTopology[expr_, lmoms_List, OptionsPattern[]]:=
 
 
 		If[	OptionValue[Tally],
+			(* TODO, here we would need a better sorting algorithm. *)
 			res = Sort[Tally[Flatten[tmp]],(#1[[1]]>#2[[1]])&];
 
 			(*	This extra check should catch things like SFAD[{{0, p1.q}, {0, 1}, -1}] SFAD[{{0, p1.q}, {0, 1}, 2}].	*)
@@ -341,6 +357,54 @@ FCLoopBasisIntegralToTopology[expr_, lmoms_List, OptionsPattern[]]:=
 				Abort[]
 		];
 
+
+		If[	OptionValue[FCE],
+			res = FCE[res]
+		];
+
+		res
+	];
+
+
+
+FCLoopBasisCreateScalarProducts[lmoms_List, extmoms_List, dims_List, head_Symbol, OptionsPattern[]] :=
+	Block[{tmp, res, dummyDim, repRule},
+
+		If[	Length[lmoms] === 0,
+			Message[FCLoopBasisCreateScalarProducts::failmsg, "The list of loop momenta may not be empty."];
+			Abort[]
+		];
+
+		If[	Length[dims] === 0,
+			Message[FCLoopBasisCreateScalarProducts::failmsg, "The list of the dimensions may not be empty."];
+			Abort[]
+		];
+
+		tmp = Join[{#, #} & /@ lmoms, Subsets[lmoms, {2}], Flatten[Thread[List[extmoms, #]] & /@ lmoms, 1]];
+
+		If[	Length[tmp] =!=FCLoopBasisGetSize[Length[lmoms], Length[extmoms]],
+			Message[FCLoopBasisCreateScalarProducts::failmsg, "The number of the obtained scalar products is incorrect."];
+			Abort[]
+		];
+
+		If[	!MatchQ[tmp, {{_, _} ..}],
+			Message[FCLoopBasisCreateScalarProducts::failmsg,"The list of the obtained scalar products is incorrect."];
+			Print[tmp];
+			Abort[]
+		];
+
+		Which[
+			head === Pair,
+				res = Map[Pair[Momentum[#[[1]], dummyDim], Momentum[#[[2]], dummyDim ]] &, tmp],
+			head === CartesianPair,
+				res = Map[CartesianPair[CartesianMomentum[#[[1]], dummyDim], CartesianMomentum[#[[2]], dummyDim ]] &, tmp],
+			True,
+				Message[FCLoopBasisCreateScalarProducts::failmsg, "Unknown scalar product head."];
+				Abort[]
+		];
+
+		repRule = List /@ Thread[Rule[dummyDim, dims]];
+		res = Flatten[res /. repRule];
 
 		If[	OptionValue[FCE],
 			res = FCE[res]
@@ -465,11 +529,10 @@ FCLoopBasisExtract[sps_. fad_FeynAmpDenominator, loopmoms_List, OptionsPattern[]
 		];
 
 		(* all possible scalar products of loop momenta among themselves and with external momenta *)
-		coeffs = Union[Join[Flatten[Outer[spd, lmoms, extmoms]], Flatten[Outer[spd, lmoms, lmoms]]]];
 
 		If[	TrueQ[!isCartesian],
-			coeffs = Union[Flatten[coeffs/.spd[a_,b_]:>(Pair[Momentum[a,#],Momentum[b,#]]&/@availableDims)]],
-			coeffs = Union[Flatten[coeffs/.spd[a_,b_]:>(CartesianPair[CartesianMomentum[a,#],CartesianMomentum[b,#]]&/@availableDims)]]
+			coeffs = Sort[FCLoopBasisCreateScalarProducts[lmoms,extmoms,availableDims,Pair]],
+			coeffs = Sort[FCLoopBasisCreateScalarProducts[lmoms,extmoms,availableDims,CartesianPair]]
 		];
 
 		(* 	Now we have all the polynomials that appear in the loop integral.
