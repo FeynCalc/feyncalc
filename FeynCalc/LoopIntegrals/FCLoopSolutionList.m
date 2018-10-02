2caa6cfbb332c15a23fc6e7187cdcab5feacce92
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2018-10-02 21:07:24 +0800

    Improved FCLoopCanonicalize and FCLoopSolutionList to support Cartesian integrals.

--- FeynCalc/LoopIntegrals/FCLoopSolutionList.m
+++ FeynCalc/LoopIntegrals/FCLoopSolutionList.m
@@ -23,14 +23,9 @@ internal function that uses the output of FCLoopCanonicalize and the list of \
 simplified integrals solsList to create the substitution list of type \
 \"Integral\" -> \"simplified Integral\"."
 
-FCLoopSolutionList::fail=
-"FCLoopSolutionList failed to create a list of solutions out of the given \
-integrals";
-
-FCLoopSolutionList::indexmissmatch=
-"FCLoopSolutionList found that the Lorentz indices in the list of solutions \
-are different from those in the list of unique integrals. `1` doesn't match `2` and
-`3` is not zero!";
+FCLoopSolutionList::failmsg =
+"Error! FCLoopSolutionList has encountered a fatal problem and must abort the computation. \
+The problem reads: `1`"
 
 
 Begin["`Package`"]
@@ -46,44 +41,35 @@ FCLoopSolutionList[{loopList_List, reversedRepIndexList_List,canIndexList_List,
 	uniqueCanIndexList_List}, solsList_List, OptionsPattern[]] :=
 	Block[{repSolList, reducedLoopList, finalRepList,z1,z2,z3},
 
-		If[	!FreeQ2[{loopList,solsList}, FeynCalc`Package`NRStuff],
-			Message[FeynCalc::nrfail];
-			Abort[]
-		];
-
 	(*	Small cross-check	*)
 	If[	Length[uniqueCanIndexList]=!=Length[solsList] ||
 		Length[canIndexList]=!=Length[reversedRepIndexList],
-		Message[FCLoopSolutionList::fail];
-		Print["1"];
+		Message[FCLoopSolutionList::failmsg, "The input is inconsistent."];
 		Abort[]
 	];
-	(* 	Also check that the solutions have the same Lorentz indices
-		as the integrals for which they will be substituted *)
-	Inner[If[	((z1=Union@Sort@Cases[#1, LorentzIndex[_, _ : 4], Infinity])=!=
-				(z2=Union@Sort@Cases[#2, LorentzIndex[_, _ : 4], Infinity])) && !MatchQ[z3=#2,0 | _[0] ],
-				Message[FCLoopSolutionList::indexmissmatch,z1,z2,z3];
-				Abort[]
-		]&,uniqueCanIndexList,solsList];
 
-	(*	This is the replacement list Integral -> Solution for the integrals
-		with canonicalized indices	*)
+
+	Inner[If[((z1=Union@Sort@Cases[#1, (CartesianIndex|LorentzIndex)[_, ___], Infinity])=!=
+			(z2=Union@Sort@Cases[#2, (CartesianIndex|LorentzIndex)[_, ___], Infinity])) && !MatchQ[z3=#2,0 | _[0] ],
+			Message[FCLoopSolutionList::failmsg,"The indices in the solutions list are different from those in the unique integrals list."];
+			Abort[]]&,uniqueCanIndexList,solsList];
+
+	(*	This is the replacement list Integral -> Solution for the integrals	with canonicalized indices.	*)
 	repSolList =
 		MapIndexed[(Rule[#1, First[solsList[[#2]]]]) &,
 		uniqueCanIndexList];
-	(*	This is the more general replacement list for integrals with
-		indices that are not canonicalized*)
+
+	(*	This is the more general replacement list for integrals with indices that are not canonicalized	*)
 	reducedLoopList = MapIndexed[((#1 /.First[(reversedRepIndexList[[#2]])])) &, (canIndexList /.repSolList)];
-	(*This is the final replacement list Original integral -> Reduced integral*)
+
+	(*	This is the final replacement list Original integral -> Reduced integral	*)
 	finalRepList =
 	MapIndexed[(Rule[#1, First[reducedLoopList[[#2]]]]) &, loopList];
 
-	(*	Check also that the final replacement list contains no
-		canonicalized indices	*)
-	If[	!FreeQ2[finalRepList,
-		Union[Cases[canIndexList, LorentzIndex[_, _ : 4], Infinity]]],
-		Message[FCLoopSolutionList::fail];
-		Print["3"];
+	(*	Check also that the final replacement list contains no canonicalized indices	*)
+	If[	!FreeQ2[finalRepList, Union[Cases[canIndexList, LorentzIndex[_, _ : 4], Infinity]]] ||
+		!FreeQ2[finalRepList, Union[Cases[canIndexList, CartesianIndex[_, _ : 3], Infinity]]],
+		Message[FCLoopSolutionList::failmsg,"Failed to create a list of solutions out of the given integrals."];
 		Abort[]
 	];
 
