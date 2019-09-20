b7e8be20824df349280b4643b5b267e98b6dcff7
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2019-09-20 19:35:05 +0200

    Fixed a bug in Tr2.

--- FeynCalc/NonCommAlgebra/Tr2.m
+++ FeynCalc/NonCommAlgebra/Tr2.m
@@ -66,8 +66,8 @@ trap[y_,ops___Rule] :=
 trdifficult[y_, ops___Rule] :=
 	If[MatchQ[y, _. DOT[(a__) /; FreeQ2[{a}, {DiracGamma[5],DiracGamma[6], DiracGamma[7]}], DiracGamma[5]]],
 		treasy[Expand[ExpandScalarProduct[Contract[DiracOrder[Collect2[DiracSimplify[y, InsideDiracTrace -> True],
-		DOT, Factoring -> False]],EpsEvaluate->False]],DiracGamma],ops],
-		treasy[Expand[ExpandScalarProduct[Contract[DiracOrder[y],EpsEvaluate->False]], DiracGamma], ops]
+		DOT, Factoring -> False]],EpsContract->False]],DiracGamma],ops],
+		treasy[Expand[ExpandScalarProduct[Contract[DiracOrder[y],EpsContract->False]], DiracGamma], ops]
 	] /. treasy -> DiracTrace /. DiracTrace -> trup /. trup -> DiracTrace /. DiracTrace -> trap;
 
 Tr2[x_] :=
