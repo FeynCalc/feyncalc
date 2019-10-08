bac012204637fbefd9f3bc50bbf32967d7f1e990
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2019-10-08 16:31:43 +0200

    Fixed a bug in DiracChainCombine that did not take into account products of DCHNs in the input.

--- FeynCalc/Dirac/DiracChainCombine.m
+++ FeynCalc/Dirac/DiracChainCombine.m
@@ -26,17 +26,24 @@ End[]
 Begin["`DiracChainCombine`Private`"];
 
 optDiracGammaCombine::usage="";
+mark::usage="";
 
 rulesMain = {
-	(n1_. DiracChain[x_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]] +
-		n2_. DiracChain[y_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]])/;!optDiracGammaCombine &&
-		Denominator[n1]===1 && Denominator[n2]===1 :>
-		DiracChain[n1 x + n2 y, i1, i2],
+	(n1_. DiracChain[x_, i1: (_DiracIndex| _ExplicitDiracIndex| _Spinor), i2: (_DiracIndex| _ExplicitDiracIndex| _Spinor)] +
+		n2_. DiracChain[y_, i1: (_DiracIndex| _ExplicitDiracIndex| _Spinor), i2: (_DiracIndex| _ExplicitDiracIndex| _Spinor)])/;
+		Denominator[n1]===1 && Denominator[n2]===1 && NonCommFreeQ[{n1,n2}] && FreeQ[{n1,n2}, DiracChain] :>
+			DiracChain[mark (n1 x + n2 y), i1, i2],
 
+	(n_. DiracChain[x_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]] +
+		n_. DiracChain[y_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]])/;
+		!(Denominator[n]===1 && NonCommFreeQ[n] && FreeQ[n, DiracChain]) :>
+			n DiracChain[mark (x + y), i1, i2],
+
+	(* Works also for Expand[DiracChainExpand[DCHN[1 + GA5, i, j] DCHN[1 + GA5, k, l]]]*)
 	(n1_. DiracChain[x_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]] +
-		n2_. DiracChain[y_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]])/; optDiracGammaCombine &&
-		Denominator[n1]===1 && Denominator[n2]===1  :>
-		DiracChain[DiracGammaCombine[n1 x + n2 y], i1, i2]
+		n2_. DiracChain[y_, i1: (DiracIndex|ExplicitDiracIndex|Spinor)[__], i2: (DiracIndex|ExplicitDiracIndex|Spinor)[__]])/;
+		!(Denominator[n1]===1 && Denominator[n2]===1 && NonCommFreeQ[{n1,n2}] && FreeQ[{n1,n2}, DiracChain]) && Expand[(n1-n2)]==0 :>
+			n1 DiracChain[mark (x - y), i1, i2]
 };
 
 
@@ -58,6 +65,12 @@ DiracChainCombine[expr_, OptionsPattern[]] :=
 		];
 
 		res = ex //. Dispatch[rules];
+
+		If[	OptionValue[DiracGammaCombine],
+			res = res /. DiracChain[x_,i_,j_]/; !FreeQ[x,mark] :> DiracChain[DiracGammaCombine[x/.mark->1],i,j]
+		];
+		res = res /. mark -> 1;
+
 
 		If[	OptionValue[FCE],
 			res = FCE[res]
