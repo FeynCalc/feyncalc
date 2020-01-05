4c9407e3c78bca61f01a1e24acafdf831773b399
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2020-01-05 18:51:38 +0100

    Added documentation for FCCheckVersion.

--- FeynCalc/Shared/SharedTools.m
+++ FeynCalc/Shared/SharedTools.m
@@ -47,11 +47,11 @@ FCAntiSymmetrize::usage=
 to the variables a1, a2, ... ";
 
 FCCheckVersion::usage=
-"FCCheckVersion[major,minor,build] checks if the current version of FeynCalc
-is larger or equal than marjor.minor.build. For example, FCCheckVersion[9,3,0]
-will generate a warning (when running with the frontend) or quit kernel (when
+"FCCheckVersion[major,minor,build] checks if the current version of FeynCalc \
+is larger or equal than marjor.minor.build. For example, FCCheckVersion[9,3,0] \
+will generate a warning (when running with the frontend) or quit kernel (when \
 running without the frontend) if the loaded FeynCalc version is older than 9.3.0. \n
-Notice that this function is available only since FeynCalc 9.3";
+Notice that this function is available only since FeynCalc 9.3.";
 
 FCDuplicateFreeQ::usage=
 "FCDuplicateFreeQ[list] yields True if list contains no duplicates and False otherwise. \n
