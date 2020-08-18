ef1b3c3e1f1624493b5506c82d6a9936f08e5fbc
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2020-08-18 15:27:24 +0200

    Improved FCFeynmanParametrize to handle loop integrals with numerators.

--- FeynCalc/LoopIntegrals/FCFeynmanParametrize.m
+++ FeynCalc/LoopIntegrals/FCFeynmanParametrize.m
@@ -50,7 +50,6 @@ Options[FCFeynmanParametrize] = {
 	FinalSubstitutions		-> {},
 	Indexed					-> True,
 	Names					-> FCGV["x"],
-	Numerator				-> False,
 	Simplify				-> False,
 	Variables				-> {}
 };
@@ -61,8 +60,10 @@ FCFeynmanParametrize[expr_, lmoms_List /; ! OptionQ[lmoms], opts:OptionsPattern[
 
 
 FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ[lmoms], OptionsPattern[]] :=
-	Block[{	res, optFinalSubstitutions, dim, uPoly, fPoly, pows, mat, powsT,
-			nM,nLoops,fPow,pref,fpInt, fpPref, optFCReplaceD, vars, optVariavbles, aux, ex},
+	Block[{	res, optFinalSubstitutions, dim, uPoly, fPoly, pows, mat, powsT, propPowers,
+			propPowersHat, propPowersTilde, ppSymbols, ppSymbolsRule,
+			denPowers, zeroPowerProps, numPowers, numVars, zeroDenVars,
+			nM,nLoops,fPow,pref,fpInt, fpPref, optFCReplaceD, vars, optVariavbles, aux, ex, Q, J},
 
 		optFinalSubstitutions	= OptionValue[FinalSubstitutions];
 		dim						= OptionValue[Dimension];
@@ -94,7 +95,7 @@ FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ
 			Abort[]
 		];
 
-		{uPoly, fPoly, pows, mat} = FCSymanzikPolynomials[ex,lmoms, FCI->True,
+		{uPoly, fPoly, pows, mat, Q, J} = FCSymanzikPolynomials[ex,lmoms, FCI->True,
 			FinalSubstitutions->OptionValue[FinalSubstitutions], Names->OptionValue[Names], Indexed->OptionValue[Indexed]];
 
 
@@ -109,12 +110,50 @@ FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ
 		FCPrint[1,"FCFeynmanParametrize: Number of loops: ", nLoops, FCDoControl->fcfpVerbose];
 		FCPrint[1,"FCFeynmanParametrize: Sum of propagator powers: ", nM, FCDoControl->fcfpVerbose];
 
-		If[!MatchQ[Last[powsT],{(_Integer?Positive| _Symbol)..}] && !OptionValue[Numerator],
-			Message[FCFeynmanParametrize::failmsg,"Numerators are currently not supported."];
+		propPowers 	= Last[powsT];
+
+
+		If[	MatchQ[propPowers,{___,0,___}],
+			Message[FCFeynmanParametrize::failmsg,"Propagators cannot be raised to zero powers!"];
 			Abort[]
 		];
 
-		pref = Gamma[fPow]/(Times @@ (Gamma /@ Last[powsT]));
+		If[MatchQ[optFCReplaceD,{_Rule}],
+			propPowers = FCReplaceD[propPowers,First[optFCReplaceD]]
+		];
+
+		ppSymbols = Cases[propPowers, _Symbol, Infinity];
+		If[ppSymbols=!={},
+			ppSymbolsRule = Alternatives@@ppSymbols -> 0,
+			ppSymbolsRule = {}
+		];
+
+		propPowersHat = Max[0, -Floor[# /. ppSymbolsRule ]]&/@propPowers;
+
+		(*The tilde propagators are either symbolic or manifestly positive *)
+		propPowersTilde = propPowers + propPowersHat;
+
+		FCPrint[1,"FCFeynmanParametrize: propPowersHat: ", propPowersHat, FCDoControl->fcfpVerbose];
+		FCPrint[1,"FCFeynmanParametrize: propPowersTilde: ", propPowersTilde, FCDoControl->fcfpVerbose];
+
+		If[	!MatchQ[propPowersTilde /. ppSymbolsRule,  {_Integer?NonNegative ..}] ||
+			!MatchQ[propPowersHat,  {_Integer?NonNegative ..}],
+			Message[FCFeynmanParametrize::failmsg,"Splitting of propagator powers failed."];
+			Abort[]
+		];
+
+		zeroPowerProps = Position[propPowersTilde, 0];
+
+		If[	!MatchQ[zeroPowerProps, {{_Integer} ...}],
+			Message[FCFeynmanParametrize::failmsg, "Identification of zero-power propagators failed."];
+			Abort[]
+		];
+
+		(*remove contributions from tilded m_i that are zero *)
+		propPowers = Delete[propPowers,zeroPowerProps];
+		vars = Delete[First[powsT],zeroPowerProps];
+
+		pref = Gamma[fPow]/(Times @@ (Gamma /@ propPowers));
 
 		If[!isCartesian,
 			pref = pref*(-1)^nM
@@ -126,12 +165,49 @@ FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ
 			Abort[]
 		];
 
-		fpPref = (Times @@ Map[Power[#[[1]],(#[[3]]-1)] &, pows]);
+		If[	vars=!={},
+			fpPref = (Times @@ Map[Power[#[[1]],(#[[2]]-1)] &, Transpose[{vars,propPowers}]]),
+			fpPref = 1
+		];
+
 		fpInt =  Power[uPoly,fPow - dim/2]/Power[fPoly,fPow];
 
 		FCPrint[1,"FCFeynmanParametrize: fpPref: ", fpPref, FCDoControl->fcfpVerbose];
 		FCPrint[1,"FCFeynmanParametrize: pref: ", pref, FCDoControl->fcfpVerbose];
 		FCPrint[1,"FCFeynmanParametrize: raw fpInt: ", fpInt, FCDoControl->fcfpVerbose];
+
+
+
+		If[	!MatchQ[propPowersHat,  {0..}],
+			FCPrint[1,"FCFeynmanParametrize: Handling scalar products in the numerator.", FCDoControl->fcfpVerbose];
+			numPowers = Position[propPowersHat, x_ /; x =!= 0, {1}, Heads -> False];
+
+			If[	!MatchQ[numPowers, {{_Integer} ...}],
+				Message[FCFeynmanParametrize::failmsg, "Identification of numerator powers failed."];
+				Abort[]
+			];
+
+			numVars 		= Extract[First[powsT], numPowers];
+			zeroDenVars 	= Extract[First[powsT], zeroPowerProps];
+			propPowersHat	= Extract[propPowersHat, numPowers];
+
+			FCPrint[1,"FCFeynmanParametrize: numVars: ", numVars, FCDoControl->fcfpVerbose];
+			FCPrint[1,"FCFeynmanParametrize: zeroDenVars: ", zeroDenVars, FCDoControl->fcfpVerbose];
+			FCPrint[1,"FCFeynmanParametrize: propPowersHat: ", propPowersHat, FCDoControl->fcfpVerbose];
+
+			If[	Length[numVars]=!=Length[propPowersHat],
+				Message[FCFeynmanParametrize::failmsg, "The number of x-variables doesn't match the number of propagators with negative powers."];
+				Abort[]
+			];
+
+
+			fpInt = Fold[( (-1)^#2[[2]] * D[#1, #2]) &, fpInt, Transpose[{numVars,propPowersHat}]];
+
+			FCPrint[3,"FCFeynmanParametrize: raw fpInt after the differentiation: ", fpInt, FCDoControl->fcfpVerbose];
+			fpInt =  fpInt /. Map[Rule[#,0]&,zeroDenVars];
+			FCPrint[3,"FCFeynmanParametrize: raw fpInt after setting x[i] to zero: ", fpInt, FCDoControl->fcfpVerbose]
+
+		];
 
 		fpInt = fpInt fpPref;
 
@@ -139,7 +215,7 @@ FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ
 			fpInt = 0
 		];
 
-		vars = First[powsT];
+
 
 		(*
 			If there is only a single Feynman parameter, the integration over the Dirac delta
@@ -177,6 +253,10 @@ FCFeynmanParametrize[expr_, extra_/; Head[extra]=!=List, lmoms_List /; ! OptionQ
 			pref	= Simplify[pref, Assumptions->OptionValue[Assumptions]]
 		];
 
+		aux		= FCProductSplit[fpInt, vars];
+
+		pref	= pref aux[[1]];
+		fpInt	= aux[[2]];
 
 
 		res = {fpInt,pref,vars};
