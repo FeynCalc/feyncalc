f85ddb403a7176be9213a9ac065ec0a20de30d7e
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2021-02-06 23:03:04 +0100

    Refactored MomentumExpand and added a new Option Momentum that allows to expand only w.r.t. the given list of momenta.

--- FeynCalc/Lorentz/MomentumExpand.m
+++ FeynCalc/Lorentz/MomentumExpand.m
@@ -6,9 +6,9 @@
 
 (*
 		This software is covered by the GNU General Public License 3.
-		Copyright (C) 1990-2018 Rolf Mertig
-		Copyright (C) 1997-2018 Frederik Orellana
-		Copyright (C) 2014-2018 Vladyslav Shtabovenko
+		Copyright (C) 1990-2021 Rolf Mertig
+		Copyright (C) 1997-2021 Frederik Orellana
+		Copyright (C) 2014-2021 Vladyslav Shtabovenko
 *)
 
 (* :Summary:  Expands momenta                                               *)
@@ -19,6 +19,10 @@ MomentumExpand::usage =
 "MomentumExpand[expr] expands Momentum[a+b+ ...] in expr into \
 Momentum[a] + Momentum[b] + ... .";
 
+MomentumExpand::failmsg =
+"Error! MomentumExpand has encountered a fatal problem and must abort the computation. \
+The problem reads: `1`"
+
 (* ------------------------------------------------------------------------ *)
 
 Begin["`Package`"]
@@ -26,42 +30,90 @@ End[]
 
 Begin["`MomentumExpand`Private`"];
 pair::usage="";
+noexpand::usage="";
 
-fourvecevlin[n_?NumberQ z_, dim_:4] :=
-	n Momentum[z, dim];
+Options[MomentumExpand] = {
+	Momentum -> All
+};
 
-fourvecev[y_,dim_:4] :=
-	fourvecev[y,dim] =
-		Block[ {hold},
-			(Distribute[fourvecevlin[ Expand[y, Momentum],
-			hold[dim]]] /. hold->Identity)
+(*Fast mode*)
+MomentumExpand[(head:(Momentum|CartesianMomentum|TemporalMomentum))[args__], OptionsPattern[]]:=
+	(momSplit[head, OptionValue[Momentum]][args] /. head-> expandVec[head] /. linearizeVec -> head /. noexpand -> Identity);
+
+
+MomentumExpand[expr_List, opts:OptionsPattern[]] :=
+	MomentumExpand[#, opts]&/@expr;
+
+(*Normal mode*)
+MomentumExpand[expr_/;!MemberQ[{Momentum,CartesianMomentum,TemporalMomentum,List},Head[expr]], OptionsPattern[]] :=
+	Block[{aux,res,hold1,hold2,listOrig,listEval,repRule,optMomentum},
+
+		optMomentum = OptionValue[Momentum];
+
+		aux = expr /. hd:(Spinor|FeynAmpDenominator|PropagatorDenominator) :> hold1[hd] /.
+			hd:(Pair|CartesianPair|TemporalPair) :> hold2[hd];
+
+		If[	!FreeQ[aux, Momentum],
+			aux = momExpand[aux,Momentum,optMomentum];
+			If[	!FreeQ2[aux,{expandVec,linearizeVec,noexpand}],
+				Message[MomentumExpand::failmsg, "Something went wrong during the expansion of 4-momenta."];
+				Abort[]
+			]
 		];
 
-threevecevlin[n_?NumberQ z_, dim_:3] :=
-	n CartesianMomentum[z, dim];
-
-threevecev[y_,dim_:3] :=
-	threevecev[y,dim] =
-		Block[ {hold},
-			(Distribute[threevecevlin[ Expand[y, CartesianMomentum],
-			hold[dim]]] /. hold->Identity)
+		If[	!FreeQ[aux, CartesianMomentum],
+			aux = momExpand[aux,CartesianMomentum,optMomentum];
+			If[	!FreeQ2[aux,{expandVec,linearizeVec,noexpand}],
+				Message[MomentumExpand::failmsg, "Something went wrong during the expansion of 3-momenta."];
+				Abort[]
+			]
 		];
 
-zerovecevlin[n_?NumberQ z_] :=
-	n TemporalMomentum[z];
+		If[	!FreeQ[aux, TemporalMomentum],
+			aux = momExpand[aux,TemporalMomentum,optMomentum];
+			If[	!FreeQ2[aux,{expandVec,linearizeVec,noexpand}],
+				Message[MomentumExpand::failmsg, "Something went wrong during the expansion of 0th momentum components."];
+				Abort[]
+			]
+		];
 
-zerovecev[y_] :=
-	zerovecev[y] =
-		Distribute[zerovecevlin[Expand[y, TemporalMomentum]]];
+		res = aux /. hold2 -> Identity /. hold1 -> Identity;
+
+		res
+
+	];
+
+momExpand[ex_, head_, optMomentum_]:=
+	Block[{listOrig,listEval,repRule},
+		listOrig = Cases2[ex,head];
+		listEval = listOrig/. head -> momSplit[head, optMomentum] /. head -> expandVec[head] /. linearizeVec -> head /. noexpand -> Identity;
+		repRule = Thread[Rule[listOrig,listEval]];
+		(ex /. Dispatch[repRule])
+	];
+
+hold[]:=Sequence[];
+
+momSplit[head_, All][a__]:=
+	head[a];
+
+momSplit[head_, moms_List][a_,dim___]:=
+	Function[{x, y}, head[noexpand[x]+y, dim]] @@ FCSplit[a, moms];
+
+
+(*
+	Since the following functions cannot be normally assigned downvalues, they
+	are safe for memoization.
+*)
+
+
+expandVec[head_][y_,dim___] :=	expandVec[head][y,dim] =
+	Distribute[linearizeVec[Expand[y, head], hold[dim]]] /. hold->Identity;
+
+linearizeVec[n_?NumberQ z_, dim___] :=
+	n linearizeVec[z, dim];
 
 
 
-MomentumExpand[expr_] :=
-	expr /. Spinor -> spinor /. FeynAmpDenominator -> fad /. PropagatorDenominator -> pd /. Pair-> pair /. CartesianPair-> cpair /. TemporalPair-> tpair /.
-	Momentum -> fourvecev /. fourvecevlin -> Momentum /.
-	CartesianMomentum -> threevecev /. threevecevlin -> CartesianMomentum /.
-	TemporalMomentum -> zerovecev /. zerovecevlin -> TemporalMomentum /.
-	pair -> Pair /. cpair -> CartesianPair /. tpair -> TemporalPair /. spinor ->Spinor /. pd -> PropagatorDenominator /. fad -> FeynAmpDenominator;
 
 FCPrint[1,"MomentumExpand.m loaded."];
 End[]
