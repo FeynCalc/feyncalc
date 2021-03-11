5ce9db494d4d811b14d55d2d9025b6d4d546ad38
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2021-03-11 10:22:35 +0100

    Updated copyright years.

--- FeynCalc/Dirac/EpsChisholm.m
+++ FeynCalc/Dirac/EpsChisholm.m
@@ -6,9 +6,9 @@
 
 (*
 	This software is covered by the GNU General Public License 3.
-	Copyright (C) 1990-2020 Rolf Mertig
-	Copyright (C) 1997-2020 Frederik Orellana
-	Copyright (C) 2014-2020 Vladyslav Shtabovenko
+	Copyright (C) 1990-2021 Rolf Mertig
+	Copyright (C) 1997-2021 Frederik Orellana
+	Copyright (C) 2014-2021 Vladyslav Shtabovenko
 *)
 
 (* :Summary:  Applies the Chisholm identity backwards						*)
--- FeynCalc/Examples/Export/exportToMD.sh
+++ FeynCalc/Examples/Export/exportToMD.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This software is covered by the GNU General Public License 3.
-# Copyright (C) 1990-2020 Rolf Mertig
-# Copyright (C) 1997-2020 Frederik Orellana
-# Copyright (C) 2014-2020 Vladyslav Shtabovenko
+# Copyright (C) 1990-2021 Rolf Mertig
+# Copyright (C) 1997-2021 Frederik Orellana
+# Copyright (C) 2014-2021 Vladyslav Shtabovenko
 
 # Description:
 
