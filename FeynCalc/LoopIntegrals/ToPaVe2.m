3396fcdfb0060021e3f6645241a9d11e20de8ece
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2021-07-07 17:25:10 +0200

    Added new Option PaVeAutoOrder to ToPaVe2.

--- FeynCalc/LoopIntegrals/ToPaVe2.m
+++ FeynCalc/LoopIntegrals/ToPaVe2.m
@@ -28,28 +28,37 @@ Begin["`ToPaVe2`Private`"]
 
 
 Options[ToPaVe2] = {
+	PaVeAutoOrder -> True
 };
 
 ToPaVe2[expr_PaVe, OptionsPattern[]]:=
 	expr;
 
 ToPaVe2[expr_/;Head[expr]=!=PaVe, OptionsPattern[]] :=
-	Block[{ex},
+	Block[{ex,pave},
 
 
 
 		If[ !OptionValue[PaVe, PaVeAutoReduce],
 			ex =expr /. {
-				A0[m_, OptionsPattern[]] :> PaVe[0,{},{m}],
-				A00[m_, OptionsPattern[]] :> PaVe[0,0,{},{m}],
-				B0[pp_,m1_,m2_, OptionsPattern[]] :> PaVe[0,{pp},{m1,m2}],
-				B1[pp_,m1_,m2_, OptionsPattern[]] :> PaVe[1,{pp},{m1,m2}],
-				B00[pp_,m1_,m2_, OptionsPattern[]] :> PaVe[0,0,{pp},{m1,m2}],
-				B11[pp_,m1_,m2_, OptionsPattern[]] :> PaVe[1,1,{pp},{m1,m2}],
-				C0[p10_,p12_,p20_,m1_,m2_,m3_, OptionsPattern[]] :> PaVe[0,{p10,p12,p20},{m1,m2,m3}],
-				D0[p10_,p12_,p23_,p30_,p20_,p13_,m1_,m2_,m3_,m4_, OptionsPattern[]] :> PaVe[0,{p10,p12,p23,p30,p20,p13},{m1,m2,m3,m4}]
+				A0[m_, OptionsPattern[]] :> pave[0,{},{m}],
+				A00[m_, OptionsPattern[]] :> pave[0,0,{},{m}],
+				B0[pp_,m1_,m2_, OptionsPattern[]] :> pave[0,{pp},{m1,m2}],
+				B1[pp_,m1_,m2_, OptionsPattern[]] :> pave[1,{pp},{m1,m2}],
+				B00[pp_,m1_,m2_, OptionsPattern[]] :> pave[0,0,{pp},{m1,m2}],
+				B11[pp_,m1_,m2_, OptionsPattern[]] :> pave[1,1,{pp},{m1,m2}],
+				C0[p10_,p12_,p20_,m1_,m2_,m3_, OptionsPattern[]] :> pave[0,{p10,p12,p20},{m1,m2,m3}],
+				D0[p10_,p12_,p23_,p30_,p20_,p13_,m1_,m2_,m3_,m4_, OptionsPattern[]] :> pave[0,{p10,p12,p23,p30,p20,p13},{m1,m2,m3,m4}]
 			}
 		];
+
+		If[	OptionValue[PaVeAutoOrder]===False,
+			ex = ex/. pave[x__] :> pave[x, PaVeAutoOrder->False]
+		];
+
+		ex = ex /. pave -> PaVe;
+
+
 		ex
 	];
 
