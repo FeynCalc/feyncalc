2117cca02290cfa14fb54cfa2d23f839dbfb56c4
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2021-11-14 17:29:27 +0100

    Improved documentation for EpsChisholm.

--- FeynCalc/Documentation/Mathematica/Dirac/EpsChisholm.m
+++ FeynCalc/Documentation/Mathematica/Dirac/EpsChisholm.m
@@ -25,3 +25,26 @@
 
 LC[\[Mu],\[Nu],\[Rho],\[Sigma]]GA[\[Sigma],5]
 EpsChisholm[%]
+
+
+(* ::Text:: *)
+(*This reproduces the identities given in the Appendix A of (arXiv:2111.05153)[https://arxiv.org/abs/2111.05153]*)
+
+
+LC[\[Alpha],\[Nu],\[Beta],\[Rho]]FV[Subscript[p, 1],\[Beta]]SpinorUBar[Subscript[p, 2],SMP["m_s"]] . GA[\[Alpha],7] . SpinorV[Subscript[p, 1],SMP["m_d"]]
+%//EpsChisholm//DiracSimplify//Contract
+
+
+LC[\[Alpha],\[Nu],\[Beta],\[Rho]]FV[Subscript[p, 2],\[Beta]]SpinorUBar[Subscript[p, 2],SMP["m_s"]] . GA[\[Alpha],7] . SpinorV[Subscript[p, 1],SMP["m_d"]]
+%//EpsChisholm//DiracSimplify//Contract
+
+
+LC[\[Alpha],\[Nu],\[Gamma],\[Rho]]FV[Subscript[p, 3],\[Gamma]]SpinorUBar[Subscript[p, 3],SMP["m_s"]] . GA[\[Nu],7] . SpinorV[Subscript[p, 4],SMP["m_d"]]
+%//EpsChisholm//DiracSimplify//Contract
+
+
+LC[\[Beta],\[Gamma],\[Mu],\[Nu]]FV[Subscript[p, 2],\[Gamma]]SpinorUBar[Subscript[p, 3],SMP["m_s"]] . GA[\[Beta],7] . SpinorV[Subscript[p, 4],SMP["m_d"]]
+%//EpsChisholm//DiracSimplify//Contract
+
+
+
