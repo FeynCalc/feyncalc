48c4149ed55c24a2695ad1e975e7608ca9e9ddff
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2022-04-21 14:37:56 +0200

    Updated documentation on FeynAmpDenominator.

--- FeynCalc/Documentation/Mathematica/LoopIntegrals/FeynAmpDenominatorExplicit.m
+++ FeynCalc/Documentation/Mathematica/LoopIntegrals/FeynAmpDenominatorExplicit.m
@@ -1,8 +1,14 @@
+(* ::Package:: *)
+
  
+
+
 (* ::Section:: *)
 (*FeynAmpDenominatorExplicit*)
+
+
 (* ::Text:: *)
-(*`FeynAmpDenominatorExplicit[exp]` changes each occurence of `PropagatorDenominator[a,b]` in exp into `1/(SPD[a,a]-b^2)` and replaces `FeynAmpDenominator` by `Identity`.*)
+(*`FeynAmpDenominatorExplicit[exp]` changes each occurrence of `PropagatorDenominator[a,b]` in exp into `1/(SPD[a,a]-b^2)` and replaces `FeynAmpDenominator` by `Identity`.*)
 
 
 (* ::Subsection:: *)
@@ -11,7 +17,6 @@
 
 (* ::Text:: *)
 (*[Overview](Extra/FeynCalc.md), [FeynAmpDenominator](FeynAmpDenominator.md), [PropagatorDenominator](PropagatorDenominator.md).*)
-
 
 
 (* ::Subsection:: *)
@@ -23,3 +28,13 @@ FAD[{q,m},{q-p,0}]
 FeynAmpDenominatorExplicit[%]
 
 %//FCE//StandardForm
+
+
+(* ::Text:: *)
+(*Notice that you should never apply `FeynAmpDenominatorExplicit` to loop integrals. Denominators in a proper loop integral should be written as `FeynAmpDenominator`s. Otherwise, the given integral is assumed to have no denominators and consequently set to zero as being scaleless.*)
+
+
+TID[FVD[q,mu]FAD[{q,m},{q-p,0}],q]
+
+
+TID[FeynAmpDenominatorExplicit[FVD[q,mu]FAD[{q,m},{q-p,0}]],q]
