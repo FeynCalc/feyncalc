9e6eca26d625e94ff12ea15c71fe2c9061f7f989
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2022-07-22 07:25:23 +0200

    Fixed a typo in the docu page for CProductSplit.

--- FeynCalc/Documentation/Mathematica/Shared/Tools/FCProductSplit.m
+++ FeynCalc/Documentation/Mathematica/Shared/Tools/FCProductSplit.m
@@ -8,7 +8,7 @@
 
 
 (* ::Text:: *)
-(*`FCProductSplit[exp, {v1, v2, ...}]` splits expr into pieces that are free of any occurrence of `v1, v2, ...` and pieces that contain those variables. This works both on sums and products. The output is provided in the form of a two element list. One can recover the original expression by applying `Total` to that list.*)
+(*`FCProductSplit[exp, {v1, v2, ...}]` splits expr into pieces that are free of any occurrence of `v1, v2, ...` and pieces that contain those variables. This works both only for products. The output is provided in the form of a two element list. One can recover the original expression by applying `Times` to that list.*)
 
 
 (* ::Subsection:: *)
