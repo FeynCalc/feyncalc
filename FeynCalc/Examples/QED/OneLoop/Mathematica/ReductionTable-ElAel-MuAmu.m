c28ee69a214d90528c94ab6e8f213fa9a2d720a1
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2024-07-12 14:07:51 +0200

    Added two reworked 1-loop examples using the new multiloop functionality.

---
+++ FeynCalc/Examples/QED/OneLoop/Mathematica/ReductionTable-ElAel-MuAmu.m
@@ -0,0 +1,65 @@
+{GLI["fctopology1", {1, 1, 1, 1}] -> GLI["fctopology1", {1, 1, 1, 1}], 
+ GLI["fctopology1", {1, 1, -2, 1}] -> 
+  (((-2*D + D^2)*t^2 + (-8*D + 2*D^2)*t*u + (8 - 6*D + D^2)*u^2)*
+    GLI["fctopology1", {0, 1, 0, 1}])/((16 - 12*D + 2*D^2)*t + 
+    (16 - 12*D + 2*D^2)*u), GLI["fctopology1", {1, 1, -1, 1}] -> 
+  (((-2 + D)*t + (-4 + D)*u)*GLI["fctopology1", {0, 1, 0, 1}])/
+   ((-4 + D)*t + (-4 + D)*u), GLI["fctopology1", {1, 1, 0, 1}] -> 
+  ((-6 + 2*D)*GLI["fctopology1", {0, 1, 0, 1}])/((-4 + D)*t + (-4 + D)*u), 
+ GLI["fctopology1", {1, 1, 1, -1}] -> 
+  (((-2 + D)*t + 2*u)*GLI["fctopology1", {1, 0, 1, 0}])/((-4 + D)*t), 
+ GLI["fctopology1", {1, 1, 1, 0}] -> 
+  ((6 - 2*D)*GLI["fctopology1", {1, 0, 1, 0}])/((-4 + D)*t), 
+ GLI["fctopology1", {-1, 1, 1, 1}] -> 
+  (((-2 + D)*t + (-4 + D)*u)*GLI["fctopology1", {0, 1, 0, 1}])/
+   ((-4 + D)*t + (-4 + D)*u), GLI["fctopology1", {0, 1, 1, 1}] -> 
+  ((-6 + 2*D)*GLI["fctopology1", {0, 1, 0, 1}])/((-4 + D)*t + (-4 + D)*u), 
+ GLI["fctopology1", {1, -1, 1, 1}] -> 
+  (((-2 + D)*t + 2*u)*GLI["fctopology1", {1, 0, 1, 0}])/((-4 + D)*t), 
+ GLI["fctopology1", {1, 0, 1, 1}] -> 
+  ((6 - 2*D)*GLI["fctopology1", {1, 0, 1, 0}])/((-4 + D)*t), 
+ GLI["fctopology1", {0, 1, -2, 1}] -> 
+  ((D*t^2 + 2*D*t*u + D*u^2)*GLI["fctopology1", {0, 1, 0, 1}])/(-4 + 4*D), 
+ GLI["fctopology1", {0, 1, -1, 1}] -> 
+  ((t + u)*GLI["fctopology1", {0, 1, 0, 1}])/2, 
+ GLI["fctopology1", {-1, 1, 0, 1}] -> 
+  ((t + u)*GLI["fctopology1", {0, 1, 0, 1}])/2, 
+ GLI["fctopology1", {0, 1, 0, 1}] -> GLI["fctopology1", {0, 1, 0, 1}], 
+ GLI["fctopology1", {1, 0, 1, 0}] -> GLI["fctopology1", {1, 0, 1, 0}], 
+ GLI["fctopology1", {1, 1, 0, 0}] -> 0, GLI["fctopology1", {1, 1, 0, -1}] -> 
+  0, GLI["fctopology1", {1, 1, -1, 0}] -> 0, 
+ GLI["fctopology1", {1, 0, 0, 1}] -> 0, GLI["fctopology1", {1, 0, 0, 0}] -> 
+  0, GLI["fctopology1", {1, 0, -1, 1}] -> 0, 
+ GLI["fctopology1", {1, -1, 0, 1}] -> 0, GLI["fctopology1", {0, 1, 1, 0}] -> 
+  0, GLI["fctopology1", {0, 1, 0, 0}] -> 0, 
+ GLI["fctopology1", {0, 1, 0, -1}] -> 0, GLI["fctopology1", {0, 1, -1, 0}] -> 
+  0, GLI["fctopology1", {0, 0, 1, 1}] -> 0, 
+ GLI["fctopology1", {0, 0, 0, 1}] -> 0, GLI["fctopology1", {0, 0, 0, 0}] -> 
+  0, GLI["fctopology1", {0, 0, -1, 1}] -> 0, 
+ GLI["fctopology1", {0, -1, 0, 1}] -> 0, GLI["fctopology2", {1, 1, 1, 1}] -> 
+  GLI["fctopology2", {1, 1, 1, 1}], GLI["fctopology2", {1, 1, -1, 1}] -> 
+  (((-4 + D)*t + (-2 + D)*u)*GLI["fctopology2", {0, 1, 0, 1}])/
+   ((-4 + D)*t + (-4 + D)*u), GLI["fctopology2", {1, 1, 0, 1}] -> 
+  ((-6 + 2*D)*GLI["fctopology2", {0, 1, 0, 1}])/((-4 + D)*t + (-4 + D)*u), 
+ GLI["fctopology2", {1, 1, 1, -1}] -> 
+  ((2*t + (-2 + D)*u)*GLI["fctopology2", {1, 0, 1, 0}])/((-4 + D)*u), 
+ GLI["fctopology2", {1, 1, 1, 0}] -> 
+  ((6 - 2*D)*GLI["fctopology2", {1, 0, 1, 0}])/((-4 + D)*u), 
+ GLI["fctopology2", {-1, 1, 1, 1}] -> 
+  (((-4 + D)*t + (-2 + D)*u)*GLI["fctopology2", {0, 1, 0, 1}])/
+   ((-4 + D)*t + (-4 + D)*u), GLI["fctopology2", {0, 1, 1, 1}] -> 
+  ((-6 + 2*D)*GLI["fctopology2", {0, 1, 0, 1}])/((-4 + D)*t + (-4 + D)*u), 
+ GLI["fctopology2", {1, -1, 1, 1}] -> 
+  ((2*t + (-2 + D)*u)*GLI["fctopology2", {1, 0, 1, 0}])/((-4 + D)*u), 
+ GLI["fctopology2", {1, 0, 1, 1}] -> 
+  ((6 - 2*D)*GLI["fctopology2", {1, 0, 1, 0}])/((-4 + D)*u), 
+ GLI["fctopology2", {0, 1, -1, 1}] -> 
+  ((t + u)*GLI["fctopology2", {0, 1, 0, 1}])/2, 
+ GLI["fctopology2", {0, 1, 0, 1}] -> GLI["fctopology2", {0, 1, 0, 1}], 
+ GLI["fctopology2", {1, 0, 1, 0}] -> GLI["fctopology2", {1, 0, 1, 0}], 
+ GLI["fctopology2", {1, 1, 0, 0}] -> 0, GLI["fctopology2", {1, 0, 0, 1}] -> 
+  0, GLI["fctopology2", {0, 1, 1, 0}] -> 0, 
+ GLI["fctopology2", {0, 1, 1, -1}] -> 0, GLI["fctopology2", {0, 1, 0, 0}] -> 
+  0, GLI["fctopology2", {0, 0, 1, 1}] -> 0, 
+ GLI["fctopology2", {0, 0, 1, 0}] -> 0, GLI["fctopology2", {0, 0, 0, 1}] -> 
+  0, GLI["fctopology2", {0, -1, 1, 1}] -> 0}
