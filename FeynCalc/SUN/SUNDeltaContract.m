bce5336d2e7551cd5d910fcc6898ea26f54eb36a
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2025-03-03 11:40:36 +0530

    Refactored SUNDeltaContract.

--- FeynCalc/SUN/SUNDeltaContract.m
+++ FeynCalc/SUN/SUNDeltaContract.m
@@ -1,13 +1,23 @@
-(* ------------------------------------------------------------------------ *)
-(* ------------------------------------------------------------------------ *)
+(* ::Package:: *)
 
-(* :Summary: Kronecker delta for SU(N) *)
+(* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *)
+
+(* :Title: SUNDeltaContract													*)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2024 Rolf Mertig
+	Copyright (C) 1997-2024 Frederik Orellana
+	Copyright (C) 2014-2024 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Kronecker delta for SU(N) in the adjoint representation   	*)
 
 (* ------------------------------------------------------------------------ *)
 
 SUNDeltaContract::usage=
 "SUNDeltaContract[exp] substitutes for all SUNDelta in exp SUNDeltaContract,
-contracts the adjoint $\\text{SU}(N)$ indices and resubstitutes SUNDelta.  
+contracts the adjoint $\\text{SU}(N)$ indices and resubstitutes SUNDelta.
 SUNDeltaContract[i, j] is the Kronecker-delta for $\\text{SU}(N)$ in the
 adjoint representation with contraction properties. It wraps the head SUNIndex
 around its arguments.";
@@ -21,49 +31,36 @@ Begin["`SUNDeltaContract`Private`"]
 
 SetAttributes[SUNDeltaContract, Orderless];
 
-(* Added check for integers - noint. F.Orellana, 11/1-2001 *)
-noint[x___] :=
-		Not[Or @@
-				Join[IntegerQ /@ {x}, IntegerQ /@
-	({x} /. {SUNIndex -> Identity, ExplicitSUNIndex -> Identity})]];
+SUNDeltaContract[expr_] :=
+	(expr //. SUNDelta -> SUNDeltaContract /. SUNDeltaContract -> SUNDelta)/; !FreeQ[expr,SUNDelta];
+
+SUNDeltaContract[x_, y_ ] :=
+	SUNDeltaContract[SUNIndex[x], SUNIndex[y]]/;
+		FreeQ2[{x}, {SUNIndex, ExplicitSUNIndex, Pattern}] || FreeQ2[{y}, {SUNIndex, ExplicitSUNIndex, Pattern}];
 
 SUNDeltaContract[expr_] :=
-	(expr //. SUNDelta -> SUNDeltaContract /. SUNDeltaContract ->
-	SUNDelta)/; !FreeQ[expr,SUNDelta];
-
-SUNDeltaContract[x_ /; FreeQ[x, SUNIndex] && !IntegerQ[x] &&
-	FreeQ[x, ExplicitSUNIndex], y_ /; FreeQ[y, SUNIndex] &&
-	!IntegerQ[y] &&	FreeQ[y, ExplicitSUNIndex]] :=
-		SUNDeltaContract[SUNIndex[x], SUNIndex[y]];
+	expr/;FreeQ2[expr,{SUNT,SUNIndex,ExplicitSUNIndex,SUNDelta,Pattern,Sequence,BlankSequence,BlankNullSequence}];
 
 SUNDeltaContract[x_SUNIndex, x_SUNIndex] :=
-	(SUNN^2 - 1) /; noint[x];
+	(SUNN^2 - 1);
 
 SUNDeltaContract /:
 	SUNDeltaContract[j_ExplicitSUNIndex, _SUNIndex]^2 :=
-		SUNDeltaContract[ExplicitSUNIndex[j], ExplicitSUNIndex[j]];
+		SUNDeltaContract[j, j];
 
-SUNDeltaContract /: SUNDeltaContract[i_SUNIndex, j_SUNIndex]^2 :=
-										(SUNN^2 - 1) /; (i =!= j) && noint[i,j];
-
-SUNDeltaContract/:
-	SUNDeltaContract[i_SUNIndex, j_] SUNDeltaContract[a_, i_SUNIndex ] :=
-		SUNDeltaContract[a,j] /; noint[i];
+SUNDeltaContract /:
+	SUNDeltaContract[i_SUNIndex, j_SUNIndex]^2 :=
+		(SUNN^2 - 1) /; (i =!= j);
 
 SUNDeltaContract/:
-	SUNDeltaContract[a_, i_SUNIndex ] SUNDeltaContract[i_SUNIndex, j_] :=
-		SUNDeltaContract[a,j] /; noint[i];
-
-SUNDeltaContract/: SUNDeltaContract[i_SUNIndex, j_] SUNDeltaContract[i_SUNIndex, k_] :=
-		SUNDeltaContract[j,k] /; noint[i];
+	SUNDeltaContract[i_SUNIndex, j_] SUNDeltaContract[i_SUNIndex, k_ ] :=
+		SUNDeltaContract[j,k];
 
 SUNDeltaContract/:
-	SUNDeltaContract[a_, i_SUNIndex ] SUNDeltaContract[b_, i_SUNIndex ] :=
-		SUNDeltaContract[a,b] /; noint[i];
-
-SUNDeltaContract/: SUNDeltaContract[i_SUNIndex, j_SUNIndex ] y_[z__] :=
-	( y[z] /. i -> j ) /; FreeQ[y[z], _FeynArts`SumOver] &&
-	!FreeQ[y[z]//Hold, i] && FreeQ[y[z], SUNDeltaContract[__]^n_Integer?Negative] /; noint[i,j];
+	SUNDeltaContract[i_SUNIndex, j_SUNIndex ] y_[z__] :=
+		(
+		( y[z] /. {i -> j} )
+		)/; FreeQ[y[z], _FeynArts`SumOver] && !FreeQ[y[z]//Hold, i] && FreeQ[y[z], SUNDeltaContract[__]^n_Integer?Negative];
 
 FCPrint[1,"SUNDeltaContract.m loaded"];
 End[]
